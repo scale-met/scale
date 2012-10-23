58aedcb12c42d94b926a520b3597fa42d7f66d12
Seiya Nishizawa <seiya@gfd-dennou.org>
2012-10-23 13:35:58 +0900

    25m and 2.5m to 20m and 2m for coldbubble experiment

--- test/case/coldbubble/inc_index_20m240x120x2.f90
+++ test/case/coldbubble/inc_index_20m240x120x2.f90
@@ -1,39 +1,39 @@
   !-----------------------------------------------------------------------------
   !
-  !++ scale3 grid parameters (25m res)
+  !++ scale3 grid parameters (20m res)
   !
   !-----------------------------------------------------------------------------
   integer, private, parameter :: KHALO = 2 ! # of halo cells: z
   integer, private, parameter :: IHALO = 2 ! # of halo cells: x
   integer, private, parameter :: JHALO = 2 ! # of halo cells: y
 
-  real(RP), private, parameter :: DX  = 25 ! length in the main region [m]: x
-  real(RP), private, parameter :: DY  = 25 ! length in the main region [m]: y
-  real(RP), private, parameter :: DZ  = 25 ! length in the main region [m]: z
+  real(RP), private, parameter :: DX  = 20 ! length in the main region [m]: x
+  real(RP), private, parameter :: DY  = 20 ! length in the main region [m]: y
+  real(RP), private, parameter :: DZ  = 20 ! length in the main region [m]: z
 
   real(RP), private, parameter :: BUFFER_DZ =  0.0_RP ! thickness of buffer region [m]: z
   real(RP), private, parameter :: BUFFER_DX =  0.0_RP ! thickness of buffer region [m]: x
   real(RP), private, parameter :: BUFFER_DY =  0.0_RP ! thickness of buffer region [m]: y
   real(RP), private, parameter :: BUFFFACT  =  1.0_RP ! strech factor for dx/dy/dz of buffer region
 
-  integer, private, parameter :: KMAX = 192 ! # of computational cells: z
-  integer, private, parameter :: IMAX =  96 ! # of computational cells: x
+  integer, private, parameter :: KMAX = 240 ! # of computational cells: z
+  integer, private, parameter :: IMAX = 120 ! # of computational cells: x
   integer, private, parameter :: JMAX =   2 ! # of computational cells: y
 
-  integer, private, parameter :: KA   = 196 ! # of z whole cells (local, with HALO)
-  integer, private, parameter :: IA   = 100 ! # of x whole cells (local, with HALO)
+  integer, private, parameter :: KA   = 244 ! # of z whole cells (local, with HALO)
+  integer, private, parameter :: IA   = 124 ! # of x whole cells (local, with HALO)
   integer, private, parameter :: JA   =   6 ! # of y whole cells (local, with HALO)
 
   integer, private, parameter :: KS   =   3 ! start point of inner domain: z, local
-  integer, private, parameter :: KE   = 194 ! end   point of inner domain: z, local
+  integer, private, parameter :: KE   = 242 ! end   point of inner domain: z, local
   integer, private, parameter :: IS   =   3 ! start point of inner domain: x, local
-  integer, private, parameter :: IE   =  98 ! end   point of inner domain: x, local
+  integer, private, parameter :: IE   = 122 ! end   point of inner domain: x, local
   integer, private, parameter :: JS   =   3 ! start point of inner domain: y, local
   integer, private, parameter :: JE   =   4 ! end   point of inner domain: y, local
 
-  integer, private, parameter :: IJA  = 192 ! # merged inner region(tentative usage)
+  integer, private, parameter :: IJA  = 240 ! # merged inner region(tentative usage)
   integer, private, parameter :: IJS  =   1 !
-  integer, private, parameter :: IJE  = 192 !
+  integer, private, parameter :: IJE  = 240 !
 
   integer, private, parameter :: IBLOCK = 8 !
   integer, private, parameter :: JBLOCK = 2 !
--- test/case/coldbubble/inc_index_2m2400x8x2.f90
+++ test/case/coldbubble/inc_index_2m2400x8x2.f90
@@ -1,31 +1,31 @@
   !-----------------------------------------------------------------------------
   !
-  !++ scale3 grid parameters (2.5m res)
+  !++ scale3 grid parameters (2m res)
   !
   !-----------------------------------------------------------------------------
   integer, private, parameter :: KHALO = 2 ! # of halo cells: z
   integer, private, parameter :: IHALO = 2 ! # of halo cells: x
   integer, private, parameter :: JHALO = 2 ! # of halo cells: y
 
-  real(RP), private, parameter :: DX  = 2.5_RP ! length in the main region [m]: x
-  real(RP), private, parameter :: DY  = 2.5_RP ! length in the main region [m]: y
-  real(RP), private, parameter :: DZ  = 2.5_RP ! length in the main region [m]: z
+  real(RP), private, parameter :: DX  = 2.0_RP ! length in the main region [m]: x
+  real(RP), private, parameter :: DY  = 2.0_RP ! length in the main region [m]: y
+  real(RP), private, parameter :: DZ  = 2.0_RP ! length in the main region [m]: z
 
   real(RP), private, parameter :: BUFFER_DZ =  0.0_RP ! thickness of buffer region [m]: z
   real(RP), private, parameter :: BUFFER_DX =  0.0_RP ! thickness of buffer region [m]: x
   real(RP), private, parameter :: BUFFER_DY =  0.0_RP ! thickness of buffer region [m]: y
   real(RP), private, parameter :: BUFFFACT  =  1.0_RP ! strech factor for dx/dy/dz of buffer region
 
-  integer, private, parameter :: KMAX = 1920 ! # of computational cells: z
+  integer, private, parameter :: KMAX = 2400 ! # of computational cells: z
   integer, private, parameter :: IMAX =    8 ! # of computational cells: x
   integer, private, parameter :: JMAX =    2 ! # of computational cells: y
 
-  integer, private, parameter :: KA   = 1924 ! # of z whole cells (local, with HALO)
+  integer, private, parameter :: KA   = 2404 ! # of z whole cells (local, with HALO)
   integer, private, parameter :: IA   =   12 ! # of x whole cells (local, with HALO)
   integer, private, parameter :: JA   =    6 ! # of y whole cells (local, with HALO)
 
   integer, private, parameter :: KS   =    3 ! start point of inner domain: z, local
-  integer, private, parameter :: KE   = 1922 ! end   point of inner domain: z, local
+  integer, private, parameter :: KE   = 2402 ! end   point of inner domain: z, local
   integer, private, parameter :: IS   =    3 ! start point of inner domain: x, local
   integer, private, parameter :: IE   =   10 ! end   point of inner domain: x, local
   integer, private, parameter :: JS   =    3 ! start point of inner domain: y, local
