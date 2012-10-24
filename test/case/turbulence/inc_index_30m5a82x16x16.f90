6f27e438a2e56f64bf735eeb5311b45394306eca
Seiya Nishizawa <seiya@gfd-dennou.org>
2012-10-24 19:29:07 +0900

    change configurations for turbulence experiment

--- test/case/turbulence/inc_index_30m5a82x16x16.f90
+++ test/case/turbulence/inc_index_30m5a82x16x16.f90
@@ -1,20 +1,20 @@
   !-----------------------------------------------------------------------------
   !
-  !++ scale3 grid parameters (res. 30m)
+  !++ scale3 grid parameters (res. 30m, asp. 5)
   !
   !-----------------------------------------------------------------------------
-  real(RP), private, parameter :: DZ  = 30 ! length in the main region [m]: x,y,z
-  real(RP), private, parameter :: DX  = 30 ! length in the main region [m]: x,y,z
-  real(RP), private, parameter :: DY  = 30 ! length in the main region [m]: x,y,z
+  real(RP), private, parameter :: DZ  =  30 ! length in the main region [m]: z
+  real(RP), private, parameter :: DX  = 150 ! length in the main region [m]: x
+  real(RP), private, parameter :: DY  = 150 ! length in the main region [m]: y
 
   real(RP), private, parameter :: BUFFER_DZ = 1.E3_RP ! thickness of buffer region [m]: z
   real(RP), private, parameter :: BUFFER_DX = 0.0_RP ! thickness of buffer region [m]: x
   real(RP), private, parameter :: BUFFER_DY = 0.0_RP ! thickness of buffer region [m]: y
   real(RP), private, parameter :: BUFFFACT  = 1.1_RP ! strech factor for dx/dy/dz of buffer region
 
-  integer, private, parameter :: KMAX  =  80 ! # of computational cells: z
-  integer, private, parameter :: IMAX  =   8 ! # of computational cells: x
-  integer, private, parameter :: JMAX  =   8 ! # of computational cells: y
+  integer, private, parameter :: KMAX  = 82 ! # of computational cells: z
+  integer, private, parameter :: IMAX  = 16 ! # of computational cells: x
+  integer, private, parameter :: JMAX  = 16 ! # of computational cells: y
 
   integer, private, parameter :: IBLOCK = IMAX !
   integer, private, parameter :: JBLOCK = JMAX !
