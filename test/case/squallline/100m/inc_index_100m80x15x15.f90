bd7986276106155b1befcf2dfc8fa112674a122b
Seiya Nishizawa <seiya@gfd-dennou.org>
2013-01-18 20:56:12 +0900

    change configuration for squall line experiment

--- test/case/squallline/100m/inc_index_100m80x15x15.f90
+++ test/case/squallline/100m/inc_index_100m80x15x15.f90
@@ -7,14 +7,14 @@
   real(RP), private, parameter :: DX  = 1000 ! length in the main region [m]: x
   real(RP), private, parameter :: DY  = 1000 ! length in the main region [m]: y
 
-  real(RP), private, parameter :: BUFFER_DZ =  13.5E3_RP ! thickness of buffer region [m]: z
-  real(RP), private, parameter :: BUFFER_DX =     0.0_RP ! thickness of buffer region [m]: x
+  real(RP), private, parameter :: BUFFER_DZ =  20.0E3_RP ! thickness of buffer region [m]: z
+  real(RP), private, parameter :: BUFFER_DX =  20.0E3_RP ! thickness of buffer region [m]: x
   real(RP), private, parameter :: BUFFER_DY =     0.0_RP ! thickness of buffer region [m]: y
   real(RP), private, parameter :: BUFFFACT  =    1.05_RP ! strech factor for dx/dy/dz of buffer region
 
-  integer, private, parameter :: KMAX = 60 ! # of computational cells: z
-  integer, private, parameter :: IMAX = 30 ! # of computational cells: x
-  integer, private, parameter :: JMAX = 30 ! # of computational cells: y
+  integer, private, parameter :: KMAX = 80 ! # of computational cells: z
+  integer, private, parameter :: IMAX = 15 ! # of computational cells: x
+  integer, private, parameter :: JMAX = 15 ! # of computational cells: y
 
   integer, private, parameter :: IBLOCK = IMAX !
   integer, private, parameter :: JBLOCK = JMAX !
