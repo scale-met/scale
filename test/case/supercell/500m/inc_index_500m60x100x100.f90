065b626d9fdfdc93bceddcd536526a6dd408192f
Hisashi Yashiro <h.yashiro@riken.jp>
2013-05-24 17:54:32 +0900

    Modify supercell test, 500m

--- test/case/supercell/500m/inc_index_500m60x100x100.f90
+++ test/case/supercell/500m/inc_index_500m60x100x100.f90
@@ -1,21 +1,21 @@
 
   !-----------------------------------------------------------------------------
   !
-  !++ scale3 grid parameters (500m res., 17km isotropic, 22km model top)
+  !++ scale3 grid parameters (500m res., 12km isotropic, 12km model top)
   !
   !-----------------------------------------------------------------------------
-  integer,  private, parameter :: KMAX =   40 ! # of computational cells: z
-  integer,  private, parameter :: IMAX =  120 ! # of computational cells: x
-  integer,  private, parameter :: JMAX =  120 ! # of computational cells: y
+  integer,  private, parameter :: KMAX =   60 ! # of computational cells: z
+  integer,  private, parameter :: IMAX =  100 ! # of computational cells: x
+  integer,  private, parameter :: JMAX =  100 ! # of computational cells: y
 
-  integer,  private, parameter :: IBLOCK = 40 ! block size for cache blocking: x
-  integer,  private, parameter :: JBLOCK = 40 ! block size for cache blocking: y
+  integer,  private, parameter :: IBLOCK = 100 ! block size for cache blocking: x
+  integer,  private, parameter :: JBLOCK = 100 ! block size for cache blocking: y
 
-  real(RP), private, save      :: DZ        =  250.0_RP ! length in the main region [m]: z
+  real(RP), private, save      :: DZ        =  200.0_RP ! length in the main region [m]: z
   real(RP), private, save      :: DX        =  500.0_RP ! length in the main region [m]: x
   real(RP), private, save      :: DY        =  500.0_RP ! length in the main region [m]: y
 
-  real(RP), private, save      :: BUFFER_DZ = 5000.0_RP ! thickness of buffer region [m]: z
+  real(RP), private, save      :: BUFFER_DZ = 2000.0_RP ! thickness of buffer region [m]: z
   real(RP), private, save      :: BUFFER_DX =    0.0_RP ! thickness of buffer region [m]: x
   real(RP), private, save      :: BUFFER_DY =    0.0_RP ! thickness of buffer region [m]: y
   real(RP), private, save      :: BUFFFACT  =    1.1_RP ! strech factor for dx/dy/dz of buffer region
