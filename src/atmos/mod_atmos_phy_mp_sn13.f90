aa615787fd636f303e83583d358709f899a35103
Yousuke Sato <yousuke.sato@riken.jp>
2013-08-04 10:49:21 +0900

    bug fix in ice nucleation suggested by Tseiki

--- src/atmos/mod_atmos_phy_mp_sn13.f90
+++ src/atmos/mod_atmos_phy_mp_sn13.f90
@@ -2667,7 +2667,7 @@ contains
              if( inucl_w ) then
               PNIccn(k,i,j) = min(dni_max, c_in*bm_M92*nm_M92*0.3_RP*exp(0.3_RP*bm_M92*(wssi-0.1_RP))*wdssi)
              else 
-              PNIccn(k,i,j) = min(dni_max, c_in*bm_M92*nm_M92*0.3_RP*exp(0.3_RP*bm_M92*(wssi-0.1_RP)))
+                PNIccn(k,i,j) = min(dni_max, max(c_in*nm_M92*exp(0.3_RP*bm_M92*(wssi-0.1_RP) )-NI(k,i,j),0.d0 )*rdt )
              endif 
              PLIccn(k,i,j) = min(dli_max, PNIccn(k,i,j)*xi_ccn )
              ! only for output
