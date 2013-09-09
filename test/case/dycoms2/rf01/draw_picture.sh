c0c0a5f1bedde804acc6bfaabdd0b23e400f122b
Yousuke Sato <yousuke.sato@riken.jp>
2013-09-09 20:56:34 +0900

    modify setting of test case of DYCOMS rf01

--- test/case/dycoms2/rf01/draw_picture.sh
+++ test/case/dycoms2/rf01/draw_picture.sh
@@ -1,8 +1,8 @@
 #! /bin/bash -x
 echo "==== POST PROCESS TO CREATE PICTURE ======="
+make analy
 .libs/profile_maker
 
-mkdir figure
 #---plot LWP by gnuplot
 gnuplot << EOF
 set terminal postscript color
@@ -14,7 +14,7 @@ plot "comp_time_evolv.txt" u 1:2 w l lw 3 t "LWP"
 set output "LWP.ps"
 replot
 EOF
-convert LWP.ps figure/LWP.jpeg
+convert LWP.ps LWP.png
 rm LWP.ps
 #---plot ql by gnuplot
 gnuplot << EOF
@@ -28,7 +28,7 @@ plot "comp_profile.txt" u 3:1 w l lw 5 t "LWP"
 set output "ql.ps"
 replot
 EOF
-convert ql.ps figure/ql.jpeg
+convert ql.ps ql.png
 rm ql.ps
 #---plot qt by gnuplot
 gnuplot << EOF
@@ -42,11 +42,40 @@ plot "comp_profile.txt" u 2:1 w l lw 5 t "LWP"
 set output "qt.ps"
 replot
 EOF
-convert qt.ps figure/qt.jpeg
+convert qt.ps qt.png
 rm qt.ps
+#---plot ww by gnuplot
+gnuplot << EOF
+set terminal postscript color
+set size square
+set grid
+set yrange [0:1200]
+set xlabel "Variance of w' [m2/s2]"
+set ylabel "Height [m]"
+plot "comp_profile.txt" u 6:1 w l lw 5 t "w'w'"
+set output "ww.ps"
+replot
+EOF
+convert ww.ps ww.png
+rm ww.ps
+#---plot gr-TKE by gnuplot
+gnuplot << EOF
+set terminal postscript color
+set size square
+set grid
+set yrange [0:1200]
+set xlabel "Variance of w' [m2/s2]"
+set ylabel "Height [m]"
+plot "comp_profile.txt" u 4:1 w l lw 5 t "w'w'"
+set output "gr_tke.ps"
+replot
+EOF
+convert gr_tke.ps gr_tke.png
+rm gr_tke.ps
 
-echo "draw QHYD at t=7200s"
-gpview history.pe00000\*.nc@QHYD,time=7200 --mean x --nocont --aspect 1 --wsn 2
-convert dcl.ps figure/qhyd_t7200.jpeg
+
+echo "draw QHYD at t=3600s"
+gpview history.pe00000\*.nc@QHYD,time=3600 --mean x --nocont --aspect 1 --wsn 2
+convert dcl.ps qhyd_t3600.png
 rm dcl.ps
 echo "==== POST PROCESS is finished ========="
