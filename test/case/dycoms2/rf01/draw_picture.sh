faecf8ca439efd5ce5c4733f4155667682617aa1
Yousuke Sato <yousuke.sato@riken.jp>
2013-09-10 20:37:48 +0900

    add -rotate 90 in draw_picture

--- test/case/dycoms2/rf01/draw_picture.sh
+++ test/case/dycoms2/rf01/draw_picture.sh
@@ -14,7 +14,7 @@ plot "comp_time_evolv.txt" u 1:2 w l lw 3 t "LWP"
 set output "LWP.ps"
 replot
 EOF
-convert LWP.ps LWP.png
+convert LWP.ps -rotate 90 LWP.png
 rm LWP.ps
 #---plot ql by gnuplot
 gnuplot << EOF
@@ -28,7 +28,7 @@ plot "comp_profile.txt" u 3:1 w l lw 5 t "LWP"
 set output "ql.ps"
 replot
 EOF
-convert ql.ps ql.png
+convert ql.ps -rotate 90 ql.png
 rm ql.ps
 #---plot qt by gnuplot
 gnuplot << EOF
@@ -42,7 +42,7 @@ plot "comp_profile.txt" u 2:1 w l lw 5 t "LWP"
 set output "qt.ps"
 replot
 EOF
-convert qt.ps qt.png
+convert qt.ps -rotate 90 qt.png
 rm qt.ps
 #---plot ww by gnuplot
 gnuplot << EOF
@@ -56,7 +56,7 @@ plot "comp_profile.txt" u 6:1 w l lw 5 t "w'w'"
 set output "ww.ps"
 replot
 EOF
-convert ww.ps ww.png
+convert ww.ps -rotate 90 ww.png
 rm ww.ps
 #---plot gr-TKE by gnuplot
 gnuplot << EOF
@@ -70,12 +70,12 @@ plot "comp_profile.txt" u 4:1 w l lw 5 t "w'w'"
 set output "gr_tke.ps"
 replot
 EOF
-convert gr_tke.ps gr_tke.png
+convert gr_tke.ps -rotate 90 gr_tke.png
 rm gr_tke.ps
 
 
 echo "draw QHYD at t=3600s"
 gpview history.pe00000\*.nc@QHYD,time=3600 --mean x --nocont --aspect 1 --wsn 2
-convert dcl.ps qhyd_t3600.png
+convert dcl.ps -rotate 90 qhyd_t3600.png
 rm dcl.ps
 echo "==== POST PROCESS is finished ========="
