8efef977e183b6b4c455376200ccc94d5eca17bc
Seiya Nishizawa <seiya@gfd-dennou.org>
2013-12-20 11:25:17 +0900

    fixed wrong tracer type in init.conf for the advection test

--- scale-les/test/case/advection/500m/run.sh
+++ scale-les/test/case/advection/500m/run.sh
@@ -1,17 +1,14 @@
 #! /bin/bash -x
 ################################################################################
 #
-# ------ FOR Linux64 & gnu C&fortran & openmpi -----
+# ------ FOR Linux64 & intel C&fortran & intel mpi -----
 #
 ################################################################################
 export FORT_FMT_RECL=400
 
 
-ln -sv ../../../../bin/Linux64-gnu-ompi/scale3_init_500m40x40x40_real8_heve_sn13_const_dummy_dummy_ .
-ln -sv ../../../../bin/Linux64-gnu-ompi/scale3_500m40x40x40_real8_heve_sn13_const_dummy_dummy_  .
-
 # run
-mpirun -np 6 ./scale3_init_500m40x40x40_real8_heve_sn13_const_dummy_dummy_ init.conf || exit
-mpirun -np 6 ./scale3_500m40x40x40_real8_heve_sn13_const_dummy_dummy_  run.conf  || exit
+mpirun -np 6 ./scale3_init init.conf || exit
+mpirun -np 6 ./scale3  run.conf  || exit
 
 ################################################################################
