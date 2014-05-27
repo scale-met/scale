f2f962fef4ba0a4ca1ebbbeb8056dd28f213e032
Tsuyoshi Yamaura <tyamaura@riken.jp>
2014-05-27 17:06:04 +0900

    modified absolute velocity calc. in sea roughness module

--- scalelib/src/ocean/scale_ocean_sub_roughness.F90
+++ scalelib/src/ocean/scale_ocean_sub_roughness.F90
@@ -31,20 +31,24 @@ module scale_ocean_roughness
 
   abstract interface
      subroutine rl( &
-          Z0,      &
-          Z0M,     &
-          Z0H,     &
-          Z0E,     &
-          ATM_Uabs )
+          Z0,  &
+          Z0M, &
+          Z0H, &
+          Z0E, &
+          UA,  &
+          VA,  &
+          WA   )
        use scale_precision
        use scale_grid_index
        implicit none
 
-       real(RP), intent(inout) :: Z0      (IA,JA) ! roughness length for save     [m]
-       real(RP), intent(out)   :: Z0M     (IA,JA) ! roughness length for momentum [m]
-       real(RP), intent(out)   :: Z0H     (IA,JA) ! roughness length for heat     [m]
-       real(RP), intent(out)   :: Z0E     (IA,JA) ! roughness length for moisture [m]
-       real(RP), intent(in)    :: ATM_Uabs(IA,JA) ! absolute velocity at Z1       [m/s]
+       real(RP), intent(inout) :: Z0 (IA,JA) ! roughness length for save     [m]
+       real(RP), intent(out)   :: Z0M(IA,JA) ! roughness length for momentum [m]
+       real(RP), intent(out)   :: Z0H(IA,JA) ! roughness length for heat     [m]
+       real(RP), intent(out)   :: Z0E(IA,JA) ! roughness length for moisture [m]
+       real(RP), intent(in)    :: UA (IA,JA) ! velocity u at the lowest atmospheric layer [m/s]
+       real(RP), intent(in)    :: VA (IA,JA) ! velocity v at the lowest atmospheric layer [m/s]
+       real(RP), intent(in)    :: WA (IA,JA) ! velocity w at the lowest atmospheric layer [m/s]
      end subroutine rl
   end interface
 
@@ -120,20 +124,24 @@ contains
 
   !-----------------------------------------------------------------------------
   subroutine OCEAN_roughness_miller92( &
-       Z0,      &
-       Z0M,     &
-       Z0H,     &
-       Z0E,     &
-       ATM_Uabs )
+       Z0,  &
+       Z0M, &
+       Z0H, &
+       Z0E, &
+       UA,  &
+       VA,  &
+       WA   )
     use scale_const, only: &
        GRAV => CONST_GRAV
     implicit none
 
-    real(RP), intent(inout) :: Z0      (IA,JA) ! roughness length for save     [m]
-    real(RP), intent(out)   :: Z0M     (IA,JA) ! roughness length for momentum [m]
-    real(RP), intent(out)   :: Z0H     (IA,JA) ! roughness length for heat     [m]
-    real(RP), intent(out)   :: Z0E     (IA,JA) ! roughness length for moisture [m]
-    real(RP), intent(in)    :: ATM_Uabs(IA,JA) ! absolute velocity at Z1       [m/s]
+    real(RP), intent(inout) :: Z0 (IA,JA) ! roughness length for save     [m]
+    real(RP), intent(out)   :: Z0M(IA,JA) ! roughness length for momentum [m]
+    real(RP), intent(out)   :: Z0H(IA,JA) ! roughness length for heat     [m]
+    real(RP), intent(out)   :: Z0E(IA,JA) ! roughness length for moisture [m]
+    real(RP), intent(in)    :: UA (IA,JA) ! velocity u at the lowest atomspheric layer [m/s]
+    real(RP), intent(in)    :: VA (IA,JA) ! velocity v at the lowest atomspheric layer [m/s]
+    real(RP), intent(in)    :: WA (IA,JA) ! velocity w at the lowest atomspheric layer [m/s]
 
     real(RP), parameter :: CM0   = 1.0E-3_RP ! bulk coef. for U*
     real(RP), parameter :: visck = 1.5E-5_RP ! kinematic viscosity
@@ -148,13 +156,14 @@ contains
     real(RP), parameter :: Z0ER  = 0.0E-0_RP ! rough factor          for moisture
     real(RP), parameter :: Z0ES  = 6.2E-1_RP ! smooth factor         for moisture
 
-    real(RP) :: Ustar
+    real(RP) :: Uabs, Ustar
     integer  :: i, j
     !---------------------------------------------------------------------------
 
     do j = JS, JE
     do i = IS, IE
-       Ustar = max( sqrt(CM0) * ATM_Uabs(i,j), OCEAN_roughness_Ustar_min )
+       Uabs  = sqrt( UA(i,j)**2 + VA(i,j)**2 + WA(i,j)**2 )
+       Ustar = max( sqrt(CM0) * Uabs, OCEAN_roughness_Ustar_min )
 
        Z0M(i,j) = max( Z0MI + Z0MR/GRAV*Ustar*Ustar + Z0MS*visck/Ustar, OCEAN_roughness_Z0M_min )
        Z0H(i,j) = max( Z0HI + Z0HR/GRAV*Ustar*Ustar + Z0HS*visck/Ustar, OCEAN_roughness_Z0H_min )
@@ -170,11 +179,13 @@ contains
   !> A Physics-Based Parameterization of Air-Sea Momentum Flux at High Wind Speeds
   !> and Its Impact on Hurricane Intensity Predictions, Mon. Wea. Rev., 135, 2869-2878
   subroutine OCEAN_roughness_moon07( &
-       Z0,      &
-       Z0M,     &
-       Z0H,     &
-       Z0E,     &
-       ATM_Uabs )
+       Z0,  &
+       Z0M, &
+       Z0H, &
+       Z0E, &
+       UA,  &
+       VA,  &
+       WA   )
     use scale_const, only: &
        GRAV   => CONST_GRAV,   &
        KARMAN => CONST_KARMAN
@@ -182,44 +193,44 @@ contains
        Z1 => REAL_Z1
     implicit none
 
-    real(RP), intent(inout) :: Z0      (IA,JA) ! roughness length for save     [m]
-    real(RP), intent(out)   :: Z0M     (IA,JA) ! roughness length for momentum [m]
-    real(RP), intent(out)   :: Z0H     (IA,JA) ! roughness length for heat     [m]
-    real(RP), intent(out)   :: Z0E     (IA,JA) ! roughness length for moisture [m]
-    real(RP), intent(in)    :: ATM_Uabs(IA,JA) ! absolute velocity at Z1       [m/s]
+    real(RP), intent(inout) :: Z0 (IA,JA) ! roughness length for save     [m]
+    real(RP), intent(out)   :: Z0M(IA,JA) ! roughness length for momentum [m]
+    real(RP), intent(out)   :: Z0H(IA,JA) ! roughness length for heat     [m]
+    real(RP), intent(out)   :: Z0E(IA,JA) ! roughness length for moisture [m]
+    real(RP), intent(in)    :: UA (IA,JA) ! velocity u at the lowest atomspheric layer [m/s]
+    real(RP), intent(in)    :: VA (IA,JA) ! velocity v at the lowest atomspheric layer [m/s]
+    real(RP), intent(in)    :: WA (IA,JA) ! velocity w at the lowest atomspheric layer [m/s]
 
     real(RP), parameter :: visck  = 1.5E-5_RP ! kinematic viscosity
     integer,  parameter :: itelim = 10        ! maximum iteration number
 
     real(RP) :: Ustar(IA,JA)
-    real(RP) :: U10M
+    real(RP) :: Uabs (IA,JA)
+    real(RP) :: U10M (IA,JA)
+
     integer  :: ite
     integer  :: i, j
     !---------------------------------------------------------------------------
 
     do j = JS, JE
     do i = IS, IE
-       Z0M(i,j) = Z0(i,j)
+       Z0M (i,j) = Z0(i,j)
+       Uabs(i,j) = sqrt( UA(i,j)**2 + VA(i,j)**2 + WA(i,j)**2 )
     enddo
     enddo
 
     do ite = 1, itelim
        do j = JS, JE
        do i = IS, IE
-          Ustar(i,j) = KARMAN * ATM_Uabs(i,j) / log( Z1(i,j)/Z0M(i,j) )
-       enddo
-       enddo
+          Ustar(i,j) = KARMAN * Uabs(i,j) / log( Z1(i,j)/Z0M(i,j) )
+          U10M (i,j) = Ustar(i,j) / KARMAN * log( 10.0_RP/Z0M(i,j) )
 
-       do j = JS, JE
-       do i = IS, IE
-          U10M  = Ustar(i,j) / KARMAN * log( 10.0_RP/Z0M(i,j) )
-
-          if ( U10M <= 12.5_RP ) then
+          if ( U10M(i,j) <= 12.5_RP ) then
              Z0M(i,j) = 0.0185_RP * Ustar(i,j)**2 / GRAV
           else
-             Z0M(i,j) = 1.0E-3_RP * ( 0.085_RP * (  -0.56_RP*Ustar(i,j)*Ustar(i,j) &
-                                                 + 20.255_RP*Ustar(i,j)            &
-                                                 +  2.458_RP                       ) - 0.58_RP )
+             Z0M(i,j) = 1.0E-3_RP * ( 0.085_RP * (  -0.56_RP*Ustar(i,j)**2 &
+                                                 + 20.255_RP*Ustar(i,j)    &
+                                                 +  2.458_RP               ) - 0.58_RP )
           endif
        enddo
        enddo
@@ -231,13 +242,8 @@ contains
     do i = IS, IE
        Z0H(i,j) = min( 5.5E-5_RP * ( Z0M(i,j) * Ustar(i,j) / visck )**(-0.6_RP), 1.1E-4_RP )
        Z0E(i,j) = Z0H(i,j)
-    enddo
-    enddo
-
-    ! update and save Z0
-    do j = JS, JE
-    do i = IS, IE
-       Z0(i,j) = Z0M(i,j)
+       ! update and save Z0
+       Z0 (i,j) = Z0M(i,j)
     enddo
     enddo
 
