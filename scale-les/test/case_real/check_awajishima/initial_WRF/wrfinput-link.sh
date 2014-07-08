c6f049858095468691d1295b4cec02acd291fd93
Tsuyoshi Yamaura <tyamaura@riken.jp>
2014-07-08 22:36:40 +0900

    modified kobe real test

--- scale-les/test/case_real/check_awajishima/initial_WRF/wrfinput-link.sh
+++ scale-les/test/case_real/check_awajishima/initial_WRF/wrfinput-link.sh
@@ -2,57 +2,53 @@
 #-----------------------------------------
 #   Test Script for XXX  (Ver.0.1)
 #   2009/10/26 --- Ryuji Yoshida.
+#   2014/07/08 --- Tsuyoshi Yamaura
 #-----------------------------------------
-dir='/data3/kenshi/140507_WRFcase-exp_for_scale/test5/WRF_recca/results_gtopo30/case2/'
+dir='/home/ryoshida/scale/data/case2'
 ftype='out'
 domain='2'
-yyyy='2011'
-mm='09'
-dd1='19'
-
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_13\:00\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00000
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_13\:10\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00001
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_13\:20\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00002
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_13\:30\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00003
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_13\:40\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00004
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_13\:50\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00005
-
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_14\:00\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00006
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_14\:10\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00007
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_14\:20\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00008
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_14\:30\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00009
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_14\:40\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00010
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_14\:50\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00011
-
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_15\:00\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00012
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_15\:10\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00013
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_15\:20\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00014
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_15\:30\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00015
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_15\:40\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00016
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_15\:50\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00017
-
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_16\:00\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00018
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_16\:10\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00019
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_16\:20\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00020
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_16\:30\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00021
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_16\:40\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00022
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_16\:50\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00023
-
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_17\:00\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00024
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_17\:10\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00025
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_17\:20\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00026
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_17\:30\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00027
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_17\:40\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00028
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_17\:50\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00029
-
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_18\:00\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00030
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_18\:10\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00031
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_18\:20\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00032
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_18\:30\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00033
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_18\:40\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00034
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_18\:50\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00035
-
-ln -s ${dir}/wrf${ftype}_d0${domain}_${yyyy}-${mm}-${dd1}_19\:00\:00 ./wrf${ftype}_d0${domain}_${yyyy}-${mm}_00036
-
+#
+dt=120
+#
+# set start date (UTC)
+#
+start_year='2011'
+start_month='09'
+start_day='19'
+start_hour='13'
+start_min='00'
+start_sec='00'
+#
+# set end date (UTC)
+#
+end_year='2011'
+end_month='09'
+end_day='19'
+end_hour='19'
+end_min='00'
+end_sec='00'
+#
 #-----------------------------------------
-#EOF
+#
+fn=0
+
+start_unix_sec=`date -u -d "${start_year}-${start_month}-${start_day} ${start_hour}:${start_min}:${start_sec}" +%s`
+end_unix_sec=`date -u -d "${end_year}-${end_month}-${end_day} ${end_hour}:${end_min}:${end_sec}" +%s`
+
+unix_sec=${start_unix_sec}
+while [ ${unix_sec} -le ${end_unix_sec} ];
+do
+  year=`date -u -d "@${unix_sec}" +%Y`
+  month=`date -u -d "@${unix_sec}" +%m`
+  day=`date -u -d "@${unix_sec}" +%d`
+  hour=`date -u -d "@${unix_sec}" +%H`
+  min=`date -u -d "@${unix_sec}" +%M`
+  sec=`date -u -d "@${unix_sec}" +%S`
+
+  fmtd_fn=`printf "%05d" $fn`
+  ln -sf ${dir}/wrf${ftype}_d0${domain}_${year}-${month}-${day}_${hour}\:${min}\:${sec} \
+             ./wrf${ftype}_d0${domain}_${year}-${month}_${fmtd_fn}
+
+  fn=`expr ${fn} \+ 1`
+  unix_sec=`expr ${unix_sec} \+ ${dt}`
+done
