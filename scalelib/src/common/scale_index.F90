5b0b25b09550012bf2d0bbed2a46cfbd63c84add
Seiya Nishizawa <seiya@gfd-dennou.org>
2014-07-09 14:32:25 +0900

    add file which I forgot to add at the last commit

---
+++ scalelib/src/common/scale_index.F90
@@ -0,0 +1,50 @@
+!-------------------------------------------------------------------------------
+!> module Index
+!!
+!! @par Description
+!!          Index module
+!!
+!! @author Team SCALE
+!!
+!! @par History
+!! @li      2014-07-09 (S.Nishizawa)   [new]
+!!
+!<
+!-------------------------------------------------------------------------------
+module scale_index
+  !-----------------------------------------------------------------------------
+  !
+  !++ used modules
+  !
+  !-----------------------------------------------------------------------------
+  use scale_stdio
+  implicit none
+  private
+  !-----------------------------------------------------------------------------
+  !
+  !++ Public procedure
+  !
+  !-----------------------------------------------------------------------------
+  !
+  !++ Public parameters & variables
+  !
+  integer, public, parameter :: I_DENS     = 1
+  integer, public, parameter :: I_MOMZ     = 2
+  integer, public, parameter :: I_MOMX     = 3
+  integer, public, parameter :: I_MOMY     = 4
+  integer, public, parameter :: I_RHOT     = 5
+  integer, public, parameter :: I_QTRC     = 6
+
+  integer, public, parameter :: I_BND_DENS = 1 ! reference density     [kg/m3]
+  integer, public, parameter :: I_BND_MOMZ = 2 ! reference momentum (z) [m/s]
+  integer, public, parameter :: I_BND_MOMX = 3 ! reference momentum (x) [m/s]
+  integer, public, parameter :: I_BND_MOMY = 4 ! reference momentum (y) [m/s]
+  integer, public, parameter :: I_BND_RHOT = 5 ! reference mass-weighted potential temperature [K]
+  integer, public, parameter :: I_BND_QV   = 6 ! reference water vapor [kg/kg]
+
+  integer, public, parameter :: I_BND_SIZE = 6 ! size of array for reference quantities
+
+  character(len=H_SHORT), public :: REF_NAME(I_BND_SIZE)
+  data REF_NAME / 'DENS_ref','VELZ_ref','VELX_ref','VELY_ref','POTT_ref','QV_ref' /
+
+end module scale_index
