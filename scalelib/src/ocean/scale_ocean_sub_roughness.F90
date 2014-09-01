dc6cfa2b1af8914f0b83cb2ae4cbe199175f4379
Tsuyoshi Yamaura <tyamaura@riken.jp>
2014-09-01 18:29:31 +0900

    fixed ocean roughness length module; fixes #250 #251

--- scalelib/src/ocean/scale_ocean_sub_roughness.F90
+++ scalelib/src/ocean/scale_ocean_sub_roughness.F90
@@ -63,6 +63,10 @@ module scale_ocean_roughness
   !
   !++ Private procedure
   !
+  private :: OCEAN_roughness_const_setup
+  private :: OCEAN_roughness_miller92_setup
+  private :: OCEAN_roughness_moon07_setup
+  private :: OCEAN_roughness_const
   private :: OCEAN_roughness_miller92
   private :: OCEAN_roughness_moon07
 
@@ -72,10 +76,28 @@ module scale_ocean_roughness
   !
   character(len=H_SHORT), private :: OCEAN_roughness_TYPE = 'MILLER92' ! sea roughness length scheme
 
-  real(RP), private :: OCEAN_roughness_Ustar_min = 1.E-3_RP ! minimum fiction velocity
-  real(RP), private :: OCEAN_roughness_Z0M_min   = 1.E-5_RP ! minimum roughness length for momentum [m]
-  real(RP), private :: OCEAN_roughness_Z0H_min   = 1.E-5_RP ! minimum roughness length for heat     [m]
-  real(RP), private :: OCEAN_roughness_Z0E_min   = 1.E-5_RP ! minimum roughness length for moisture [m]
+  real(RP), private :: OCEAN_roughness_visck          = 1.5E-5_RP ! kinematic viscosity
+  real(RP), private :: OCEAN_roughness_Ustar_min      = 1.0E-3_RP ! minimum fiction velocity
+  real(RP), private :: OCEAN_roughness_Z0M_min        = 1.0E-5_RP ! minimum roughness length for momentum [m]
+  real(RP), private :: OCEAN_roughness_Z0H_min        = 1.0E-5_RP ! minimum roughness length for heat     [m]
+  real(RP), private :: OCEAN_roughness_Z0E_min        = 1.0E-5_RP ! minimum roughness length for moisture [m]
+
+  real(RP), private :: OCEAN_roughness_const_Z0M      = 1.0E-5_RP ! constant roughness length for momentum [m]
+  real(RP), private :: OCEAN_roughness_const_Z0H      = 1.0E-5_RP ! constant roughness length for momentum [m]
+  real(RP), private :: OCEAN_roughness_const_Z0E      = 1.0E-5_RP ! constant roughness length for momentum [m]
+
+  real(RP), private :: OCEAN_roughness_miller92_CM0   = 1.0E-3_RP ! bulk coef. for U*
+  real(RP), private :: OCEAN_roughness_miller92_Z0MI  = 0.0E-0_RP ! base roughness length for momentum
+  real(RP), private :: OCEAN_roughness_miller92_Z0MR  = 1.8E-2_RP ! rough factor          for momentum
+  real(RP), private :: OCEAN_roughness_miller92_Z0MS  = 1.1E-1_RP ! smooth factor         for momentum
+  real(RP), private :: OCEAN_roughness_miller92_Z0HI  = 1.4E-5_RP ! base roughness length for heat
+  real(RP), private :: OCEAN_roughness_miller92_Z0HR  = 0.0E-0_RP ! rough factor          for heat
+  real(RP), private :: OCEAN_roughness_miller92_Z0HS  = 4.0E-1_RP ! smooth factor         for heat
+  real(RP), private :: OCEAN_roughness_miller92_Z0EI  = 1.3E-4_RP ! base roughness length for moisture
+  real(RP), private :: OCEAN_roughness_miller92_Z0ER  = 0.0E-0_RP ! rough factor          for moisture
+  real(RP), private :: OCEAN_roughness_miller92_Z0ES  = 6.2E-1_RP ! smooth factor         for moisture
+
+  integer,  private :: OCEAN_roughness_moon07_itelim  = 10        ! maximum iteration number
 
   !-----------------------------------------------------------------------------
 contains
@@ -87,6 +109,7 @@ contains
 
     NAMELIST / PARAM_OCEAN_ROUGHNESS / &
        OCEAN_roughness_TYPE,      &
+       OCEAN_roughness_visck,     &
        OCEAN_roughness_Ustar_min, &
        OCEAN_roughness_Z0M_min,   &
        OCEAN_roughness_Z0H_min,   &
@@ -110,10 +133,15 @@ contains
     if( IO_LNML ) write(IO_FID_LOG,nml=PARAM_OCEAN_ROUGHNESS)
 
     select case( OCEAN_roughness_TYPE )
+    case ('CONST')
+       OCEAN_roughness => OCEAN_roughness_const
+       call OCEAN_roughness_const_setup
     case ('MILLER92')
        OCEAN_roughness => OCEAN_roughness_miller92
+       call OCEAN_roughness_miller92_setup
     case ('MOON07')
        OCEAN_roughness => OCEAN_roughness_moon07
+       call OCEAN_roughness_moon07_setup
     case default
        write(*,*) 'xxx invalid sea roughness length scheme (', trim(OCEAN_roughness_TYPE), '). CHECK!'
        call PRC_MPIstop
@@ -121,6 +149,128 @@ contains
 
     return
   end subroutine OCEAN_roughness_setup
+
+  !-----------------------------------------------------------------------------
+  subroutine OCEAN_roughness_const_setup
+    use scale_process, only: &
+       PRC_MPIstop
+    implicit none
+
+    NAMELIST / PARAM_OCEAN_ROUGHNESS_CONST / &
+       OCEAN_roughness_const_Z0M,   &
+       OCEAN_roughness_const_Z0H,   &
+       OCEAN_roughness_const_Z0E
+
+    integer :: ierr
+    !---------------------------------------------------------------------------
+
+    !--- read namelist
+    rewind(IO_FID_CONF)
+    read(IO_FID_CONF,nml=PARAM_OCEAN_ROUGHNESS_CONST,iostat=ierr)
+    if( ierr < 0 ) then !--- missing
+       if( IO_L ) write(IO_FID_LOG,*) '*** Not found namelist. Default used.'
+    elseif( ierr > 0 ) then !--- fatal error
+       write(*,*) 'xxx Not appropriate names in namelist PARAM_OCEAN_ROUGHNESS_CONST. Check!'
+       call PRC_MPIstop
+    endif
+    if( IO_LNML ) write(IO_FID_LOG,nml=PARAM_OCEAN_ROUGHNESS_CONST)
+
+    return
+  end subroutine OCEAN_roughness_const_setup
+
+  !-----------------------------------------------------------------------------
+  subroutine OCEAN_roughness_miller92_setup
+    use scale_process, only: &
+       PRC_MPIstop
+    implicit none
+
+    NAMELIST / PARAM_OCEAN_ROUGHNESS_MILLER92 / &
+       OCEAN_roughness_miller92_CM0,    &
+       OCEAN_roughness_miller92_Z0MI,   &
+       OCEAN_roughness_miller92_Z0MR,   &
+       OCEAN_roughness_miller92_Z0MS,   &
+       OCEAN_roughness_miller92_Z0HI,   &
+       OCEAN_roughness_miller92_Z0HR,   &
+       OCEAN_roughness_miller92_Z0HS,   &
+       OCEAN_roughness_miller92_Z0EI,   &
+       OCEAN_roughness_miller92_Z0ER,   &
+       OCEAN_roughness_miller92_Z0ES
+
+    integer :: ierr
+    !---------------------------------------------------------------------------
+
+    !--- read namelist
+    rewind(IO_FID_CONF)
+    read(IO_FID_CONF,nml=PARAM_OCEAN_ROUGHNESS_MILLER92,iostat=ierr)
+    if( ierr < 0 ) then !--- missing
+       if( IO_L ) write(IO_FID_LOG,*) '*** Not found namelist. Default used.'
+    elseif( ierr > 0 ) then !--- fatal error
+       write(*,*) 'xxx Not appropriate names in namelist PARAM_OCEAN_ROUGHNESS_MILLER92. Check!'
+       call PRC_MPIstop
+    endif
+    if( IO_LNML ) write(IO_FID_LOG,nml=PARAM_OCEAN_ROUGHNESS_MILLER92)
+
+    return
+  end subroutine OCEAN_roughness_miller92_setup
+
+  !-----------------------------------------------------------------------------
+  subroutine OCEAN_roughness_moon07_setup
+    use scale_process, only: &
+       PRC_MPIstop
+    implicit none
+
+    NAMELIST / PARAM_OCEAN_ROUGHNESS_MOON07 / &
+       OCEAN_roughness_moon07_itelim
+
+    integer :: ierr
+    !---------------------------------------------------------------------------
+
+    !--- read namelist
+    rewind(IO_FID_CONF)
+    read(IO_FID_CONF,nml=PARAM_OCEAN_ROUGHNESS_MOON07,iostat=ierr)
+    if( ierr < 0 ) then !--- missing
+       if( IO_L ) write(IO_FID_LOG,*) '*** Not found namelist. Default used.'
+    elseif( ierr > 0 ) then !--- fatal error
+       write(*,*) 'xxx Not appropriate names in namelist PARAM_OCEAN_ROUGHNESS_MOON07. Check!'
+       call PRC_MPIstop
+    endif
+    if( IO_LNML ) write(IO_FID_LOG,nml=PARAM_OCEAN_ROUGHNESS_MOON07)
+
+    return
+  end subroutine OCEAN_roughness_moon07_setup
+
+  !-----------------------------------------------------------------------------
+  subroutine OCEAN_roughness_const( &
+       Z0,  &
+       Z0M, &
+       Z0H, &
+       Z0E, &
+       UA,  &
+       VA,  &
+       WA   )
+    implicit none
+
+    real(RP), intent(inout) :: Z0 (IA,JA) ! roughness length for save     [m]
+    real(RP), intent(out)   :: Z0M(IA,JA) ! roughness length for momentum [m]
+    real(RP), intent(out)   :: Z0H(IA,JA) ! roughness length for heat     [m]
+    real(RP), intent(out)   :: Z0E(IA,JA) ! roughness length for moisture [m]
+    real(RP), intent(in)    :: UA (IA,JA) ! velocity u at the lowest atomspheric layer [m/s]
+    real(RP), intent(in)    :: VA (IA,JA) ! velocity v at the lowest atomspheric layer [m/s]
+    real(RP), intent(in)    :: WA (IA,JA) ! velocity w at the lowest atomspheric layer [m/s]
+
+    integer  :: i, j
+    !---------------------------------------------------------------------------
+
+    do j = 1, JA
+    do i = 1, IA
+       Z0M(i,j) = OCEAN_roughness_const_Z0M
+       Z0H(i,j) = OCEAN_roughness_const_Z0H
+       Z0E(i,j) = OCEAN_roughness_const_Z0E
+    end do
+    end do
+
+    return
+  end subroutine OCEAN_roughness_const
 
   !-----------------------------------------------------------------------------
   subroutine OCEAN_roughness_miller92( &
@@ -143,19 +293,6 @@ contains
     real(RP), intent(in)    :: VA (IA,JA) ! velocity v at the lowest atomspheric layer [m/s]
     real(RP), intent(in)    :: WA (IA,JA) ! velocity w at the lowest atomspheric layer [m/s]
 
-    real(RP), parameter :: CM0   = 1.0E-3_RP ! bulk coef. for U*
-    real(RP), parameter :: visck = 1.5E-5_RP ! kinematic viscosity
-
-    real(RP), parameter :: Z0MI  = 0.0E-0_RP ! base roughness length for momentum
-    real(RP), parameter :: Z0MR  = 1.8E-2_RP ! rough factor          for momentum
-    real(RP), parameter :: Z0MS  = 1.1E-1_RP ! smooth factor         for momentum
-    real(RP), parameter :: Z0HI  = 1.4E-5_RP ! base roughness length for heat
-    real(RP), parameter :: Z0HR  = 0.0E-0_RP ! rough factor          for heat
-    real(RP), parameter :: Z0HS  = 4.0E-1_RP ! smooth factor         for heat
-    real(RP), parameter :: Z0EI  = 1.3E-4_RP ! base roughness length for moisture
-    real(RP), parameter :: Z0ER  = 0.0E-0_RP ! rough factor          for moisture
-    real(RP), parameter :: Z0ES  = 6.2E-1_RP ! smooth factor         for moisture
-
     real(RP) :: Uabs, Ustar
     integer  :: i, j
     !---------------------------------------------------------------------------
@@ -163,11 +300,20 @@ contains
     do j = 1, JA
     do i = 1, IA
        Uabs  = sqrt( UA(i,j)**2 + VA(i,j)**2 )
-       Ustar = max( sqrt(CM0) * Uabs, OCEAN_roughness_Ustar_min )
+       Ustar = max( sqrt( OCEAN_roughness_miller92_CM0 ) * Uabs, OCEAN_roughness_Ustar_min )
 
-       Z0M(i,j) = max( Z0MI + Z0MR/GRAV*Ustar*Ustar + Z0MS*visck/Ustar, OCEAN_roughness_Z0M_min )
-       Z0H(i,j) = max( Z0HI + Z0HR/GRAV*Ustar*Ustar + Z0HS*visck/Ustar, OCEAN_roughness_Z0H_min )
-       Z0E(i,j) = max( Z0EI + Z0ER/GRAV*Ustar*Ustar + Z0ES*visck/Ustar, OCEAN_roughness_Z0E_min )
+       Z0M(i,j) = max( OCEAN_roughness_miller92_Z0MI &
+                     + OCEAN_roughness_miller92_Z0MR / GRAV * Ustar * Ustar &
+                     + OCEAN_roughness_miller92_Z0MS * OCEAN_roughness_visck / Ustar, &
+                       OCEAN_roughness_Z0M_min )
+       Z0H(i,j) = max( OCEAN_roughness_miller92_Z0HI &
+                     + OCEAN_roughness_miller92_Z0HR / GRAV * Ustar * Ustar &
+                     + OCEAN_roughness_miller92_Z0HS * OCEAN_roughness_visck / Ustar, &
+                       OCEAN_roughness_Z0H_min )
+       Z0E(i,j) = max( OCEAN_roughness_miller92_Z0EI &
+                     + OCEAN_roughness_miller92_Z0ER / GRAV * Ustar * Ustar &
+                     + OCEAN_roughness_miller92_Z0ES * OCEAN_roughness_visck / Ustar, &
+                       OCEAN_roughness_Z0E_min )
     enddo
     enddo
 
@@ -201,9 +347,6 @@ contains
     real(RP), intent(in)    :: VA (IA,JA) ! velocity v at the lowest atomspheric layer [m/s]
     real(RP), intent(in)    :: WA (IA,JA) ! velocity w at the lowest atomspheric layer [m/s]
 
-    real(RP), parameter :: visck  = 1.5E-5_RP ! kinematic viscosity
-    integer,  parameter :: itelim = 10        ! maximum iteration number
-
     real(RP) :: Ustar(IA,JA)
     real(RP) :: Uabs (IA,JA)
     real(RP) :: U10M
@@ -219,7 +362,7 @@ contains
     enddo
     enddo
 
-    do ite = 1, itelim
+    do ite = 1, OCEAN_roughness_moon07_itelim
        do j = 1, JA
        do i = 1, IA
           Ustar(i,j) = max( KARMAN * Uabs(i,j) / log( Z1(i,j)/Z0M(i,j) ), OCEAN_roughness_Ustar_min )
@@ -240,7 +383,7 @@ contains
     !  Fairall et al. (2003) JCLI, vol. 16, 571-591. Eq. (28)
     do j = 1, JA
     do i = 1, IA
-       Z0H(i,j) = min( 5.5E-5_RP / ( Z0M(i,j) * Ustar(i,j) / visck )**0.6_RP, 1.1E-4_RP )
+       Z0H(i,j) = min( 5.5E-5_RP / ( Z0M(i,j) * Ustar(i,j) / OCEAN_roughness_visck )**0.6_RP, 1.1E-4_RP )
        Z0E(i,j) = Z0H(i,j)
     enddo
     enddo
