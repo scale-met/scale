aaa9920f7428e3a6b1a28d1999f56eb04bd8db31
Sachiho Adachi <sachiho.adachi@riken.jp>
2014-12-19 21:49:39 +0900

    bugfix history output at initial step

--- scalelib/src/io/scale_history.F90
+++ scalelib/src/io/scale_history.F90
@@ -454,7 +454,8 @@ contains
     use gtool_history, only: &
        HistoryAddVariable
     use scale_time, only: &
-       TIME_STARTSEC
+       TIME_STARTSEC,     &
+       TIME_NOWDAYSEC
     implicit none
 
     integer,          intent(out) :: itemid  !< index number of the item
@@ -539,15 +540,15 @@ contains
        endif
 
     end if
-
-    call HistoryAddVariable( item,          & ! [IN]
-                             dims(1:ndim),  & ! [IN]
-                             desc,          & ! [IN]
-                             unit,          & ! [IN]
-                             TIME_STARTSEC, & ! [IN]
-                             itemid,        & ! [OUT]
-                             zinterp,       & ! [OUT]
-                             existed        ) ! [OUT]
+   
+    call HistoryAddVariable( item,           & ! [IN]
+                             dims(1:ndim),   & ! [IN]
+                             desc,           & ! [IN]
+                             unit,           & ! [IN]
+                             TIME_NOWDAYSEC, & ! [IN]
+                             itemid,         & ! [OUT]
+                             zinterp,        & ! [OUT]
+                             existed         ) ! [OUT]
 
     call PROF_rapend  ('FILE O NetCDF', 2)
 
