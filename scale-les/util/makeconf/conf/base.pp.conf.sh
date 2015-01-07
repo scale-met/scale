ef9087b220228ebc073562204b0d51d89e35b936
Tsuyoshi Yamaura <tyamaura@riken.jp>
2015-01-07 14:17:44 +0900

    added tool of making configure files for product-run

---
+++ scale-les/util/makeconf/conf/base.pp.conf.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+cat << EOF > base.pp.conf
+
+#################################################
+#
+# model configuration: pp.conf only
+#
+#################################################
+
+&PARAM_IO
+ IO_LOG_BASENAME = "${PP_IO_LOG_BASENAME}",
+/
+
+&PARAM_TOPO
+ TOPO_OUT_BASENAME = "${TOPO_OUT_BASENAME}",
+/
+
+&PARAM_LANDUSE
+ LANDUSE_OUT_BASENAME = "${LANDUSE_OUT_BASENAME}",
+/
+
+&PARAM_CONVERT
+ CONVERT_TOPO    = .true.,
+ CONVERT_LANDUSE = .true.,
+/
+
+&PARAM_CNVTOPO
+ CNVTOPO_name                = "${TOPOTYPE[$D]}",
+ CNVTOPO_SMOOTH_MAXSLOPE     = ${MAXSLOPE[$D]},
+ CNVTOPO_SMOOTH_MAXSLOPE_BND = ${MAXSLOPE_BND[$D]},
+ CNVTOPO_SMOOTH_itelim       = 10000,
+/
+
+&PARAM_CNVTOPO_${TOPOTYPE[$D]}
+ ${TOPOTYPE[$D]}_IN_CATALOGUE = "${TOPO_IN_CATALOGUE}",
+ ${TOPOTYPE[$D]}_IN_DIR       = "${TOPO_IN_DIR}",
+/
+
+&PARAM_CNVLANDUSE
+ CNVLANDUSE_name = "${LANDUSETYPE[$D]}",
+/
+
+&PARAM_CNVLANDUSE_${LANDUSETYPE[$D]}
+ ${LANDUSETYPE[$D]}_IN_CATALOGUE = "${LANDUSE_IN_CATALOGUE}",
+ ${LANDUSETYPE[$D]}_IN_DIR       = "${LANDUSE_IN_DIR}",
+/
+EOF
---
+++ scale-les/util/makeconf/conf/base.run.conf.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+cat << EOF > base.run.conf
+
+#################################################
+#
+# model configuration: run.conf only
+#
+#################################################
+
+&PARAM_IO
+ IO_LOG_BASENAME = "${RUN_IO_LOG_BASENAME}",
+/
+
+&PARAM_TIME
+ TIME_STARTDATE             = ${TIME_STARTDATE},
+ TIME_STARTMS               = ${TIME_STARTMS},
+ TIME_DURATION              = ${TIME_DURATION},
+ TIME_DURATION_UNIT         = "${TIME_UNIT}",
+ TIME_DT                    = ${TIME_DT[$D]},
+ TIME_DT_UNIT               = "${TIME_UNIT}",
+ TIME_DT_ATMOS_DYN          = ${TIME_DT_ATMOS_DYN[$D]},
+ TIME_DT_ATMOS_DYN_UNIT     = "${TIME_UNIT}",
+ TIME_DT_ATMOS_PHY_MP       = ${TIME_DT_ATMOS_PHY_MP[$D]},
+ TIME_DT_ATMOS_PHY_MP_UNIT  = "${TIME_UNIT}",
+ TIME_DT_ATMOS_PHY_RD       = ${TIME_DT_ATMOS_PHY_RD[$D]},
+ TIME_DT_ATMOS_PHY_RD_UNIT  = "${TIME_UNIT}",
+ TIME_DT_ATMOS_PHY_SF       = ${TIME_DT_ATMOS_PHY_SF[$D]},
+ TIME_DT_ATMOS_PHY_SF_UNIT  = "${TIME_UNIT}",
+ TIME_DT_ATMOS_PHY_TB       = ${TIME_DT_ATMOS_PHY_TB[$D]},
+ TIME_DT_ATMOS_PHY_TB_UNIT  = "${TIME_UNIT}",
+ TIME_DT_OCEAN              = ${TIME_DT_OCEAN[$D]},
+ TIME_DT_OCEAN_UNIT         = "${TIME_UNIT}",
+ TIME_DT_LAND               = ${TIME_DT_LAND[$D]},
+ TIME_DT_LAND_UNIT          = "${TIME_UNIT}",
+ TIME_DT_URBAN              = ${TIME_DT_URBAN[$D]},
+ TIME_DT_URBAN_UNIT         = "${TIME_UNIT}",
+ TIME_DT_ATMOS_RESTART      = ${TIME_DT_RESTART},
+ TIME_DT_ATMOS_RESTART_UNIT = "${TIME_UNIT}",
+ TIME_DT_OCEAN_RESTART      = ${TIME_DT_RESTART},
+ TIME_DT_OCEAN_RESTART_UNIT = "${TIME_UNIT}",
+ TIME_DT_LAND_RESTART       = ${TIME_DT_RESTART},
+ TIME_DT_LAND_RESTART_UNIT  = "${TIME_UNIT}",
+ TIME_DT_URBAN_RESTART      = ${TIME_DT_RESTART},
+ TIME_DT_URBAN_RESTART_UNIT = "${TIME_UNIT}",
+/
+
+&PARAM_NEST
+ USE_NESTING              = ${RUN_USE_NESTING},
+ OFFLINE                  = .false.,
+ ONLINE_DOMAIN_NUM        = ${DNUM},
+ ONLINE_IAM_PARENT        = ${IAM_PARENT},
+ ONLINE_IAM_DAUGHTER      = ${IAM_DAUGHTER},
+ ONLINE_BOUNDARY_USE_QHYD = .true.,
+ ONLINE_AGGRESSIVE_COMM   = .true.,
+/
+
+&PARAM_STATISTICS
+ STATISTICS_checktotal     = .false.,
+ STATISTICS_use_globalcomm = .false.,
+/
+
+&PARAM_RESTART
+ RESTART_OUTPUT       = .true.,
+ RESTART_OUT_BASENAME = "${RESTART_OUT_BASENAME}",
+ RESTART_IN_BASENAME  = "${RUN_RESTART_IN_BASENAME}",
+/
+
+&PARAM_TOPO
+ TOPO_IN_BASENAME = "${TOPO_IN_BASENAME}",
+/
+
+&PARAM_LANDUSE
+ LANDUSE_IN_BASENAME = "${LANDUSE_IN_BASENAME}",
+/
+
+&PARAM_LAND_PROPERTY
+ LAND_PROPERTY_IN_FILENAME = "${LAND_PROPERTY_IN_FILENAME}",
+/
+EOF
