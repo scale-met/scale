7e3184a08dcbb80b993af4b4b52a1205bf9b8da1
Tsuyoshi Yamaura <tyamaura@riken.jp>
2015-01-21 16:02:44 +0900

    updated mkconf.sh

--- scale-les/util/makeconf/mkconf.sh
+++ scale-les/util/makeconf/mkconf.sh
@@ -14,16 +14,22 @@ NUM_DOMAIN=3
 RUN_USE_NESTING=".true."
 
 # time parameters
-STARTDATE=( 1999  5 16  6  0  0  0 ) # Year Mon Day Hour Min Sec milli-Sec
-#STARTDATE=( 1999  6 10  6  0  0  0 ) # Year Mon Day Hour Min Sec milli-Sec
-#STARTDATE=( 1999  7 29  6  0  0  0 ) # Year Mon Day Hour Min Sec milli-Sec
-#STARTDATE=( 1999  8 21  6  0  0  0 ) # Year Mon Day Hour Min Sec milli-Sec
+DEF_YEAR=${1:-0000}   # Year
+DEF_MON=${2:-1}       # Mon
+DEF_DAY=${3:-1}       # Day
+DEF_HOUR=${4:-0}      # Hour
+DEF_MIN=${5:-0}       # Min
+DEF_SEC=${6:-0}       # Sec
+DEF_MSEC=${7:-0}      # milli-Sec
 
-TIME_DURATION="604800.D0"
+STARTDATE=( ${DEF_YEAR} ${DEF_MON} ${DEF_DAY} ${DEF_HOUR} ${DEF_MIN} ${DEF_SEC} ${DEF_MSEC} )
+
+TIME_DURATION="410400.D0"
 TIME_UNIT="SEC"
 
 TIME_DT_RESTART="86400.D0"
-TIME_DT_HISTORY="900.D0"
+TIME_DT_HISTORY="3600.D0"
+TIME_DT_HISTORY_HI="600.D0"
 TIME_DT_BOUNDARY="21600.D0"
 TIME_DT_REFSTATE="10800.D0"
 
@@ -55,44 +61,44 @@ DX=( "7500.0D0" "2500.0D0" "500.0D0" ) # required num. of parameters for each do
 DY=( "7500.0D0" "2500.0D0" "500.0D0" ) # required num. of parameters for each domain
 
 DEF_Z=( 
-"FZ(:) =     80.841D0,    248.821D0,    429.882D0,    625.045D0,    835.409D0,
-           1062.158D0,   1306.565D0,   1570.008D0,   1853.969D0,   2160.047D0,
-           2489.963D0,   2845.575D0,   3228.883D0,   3642.044D0,   4087.384D0,
-           4567.409D0,   5084.820D0,   5642.530D0,   6243.676D0,   6891.642D0,
-           7590.074D0,   8342.904D0,   9154.367D0,  10029.028D0,  10971.815D0,
-          11988.030D0,  13083.390D0,  14264.060D0,  15536.685D0,  16908.430D0,
-          18387.010D0,  19980.750D0,  21698.615D0,  23550.275D0,  25546.155D0,
-          28113.205D0,"
+"FZ(:) =    161.6830D0,   335.9580D0,   523.8060D0,   726.2850D0,   944.5340D0,
+           1179.7810D0,  1433.3490D0,  1706.6670D0,  2001.2720D0,  2318.8220D0,
+           2661.1040D0,  3030.0450D0,  3427.7200D0,  3856.3680D0,  4318.4000D0,
+           4816.4180D0,  5353.2230D0,  5931.8370D0,  6555.5160D0,  7227.7690D0,
+           7952.3800D0,  8733.4280D0,  9575.3060D0, 10482.7500D0, 11460.8800D0,
+          12515.1800D0, 13651.6000D0, 14876.5200D0, 16196.8500D0, 17620.0100D0,
+          19154.0100D0, 20807.4900D0, 22589.7400D0, 24510.8100D0, 26581.5000D0,
+          29644.9100D0,"
 
-"FZ(:) =    80.0000D0,   160.0000D0,   240.0000D0,   320.0000D0,   400.0000D0,
-           485.4400D0,   576.6899D0,   674.1449D0,   778.2268D0,   889.3863D0,
-          1008.1046D0,  1134.8958D0,  1270.3087D0,  1414.9298D0,  1569.3851D0,
-          1734.3434D0,  1910.5188D0,  2098.6741D0,  2299.6240D0,  2514.2385D0,
-          2743.4468D0,  2988.2412D0,  3249.6816D0,  3528.9001D0,  3827.1055D0,
-          4145.5889D0,  4485.7290D0,  4848.9985D0,  5236.9702D0,  5651.3242D0,
-          6093.8545D0,  6566.4771D0,  7071.2378D0,  7610.3223D0,  8186.0645D0,
-          8800.9570D0,  9457.6621D0, 10159.0234D0, 10908.0771D0, 11708.0664D0,
-         12508.0664D0, 13308.0664D0, 14108.0664D0, 14908.0664D0, 15708.0664D0,
-         16508.0664D0, 17308.0664D0, 18108.0664D0, 18908.0664D0, 19708.0664D0,
-         20508.0664D0, 21308.0664D0, 22108.0664D0, 22908.0664D0, 23708.0664D0,
-         24508.0664D0, 25308.0664D0, 26108.0664D0, 26908.0664D0, 27708.0664D0,"
+"FZ(:) =    110.0000D0,   220.0000D0,   330.0000D0,   440.0000D0,   550.0000D0,
+            666.1600D0,   788.8249D0,   918.3591D0,  1055.1472D0,  1199.5955D0,
+           1352.1328D0,  1513.2123D0,  1683.3123D0,  1862.9379D0,  2052.6226D0,
+           2252.9297D0,  2464.4541D0,  2687.8240D0,  2923.7026D0,  3172.7905D0,
+           3435.8274D0,  3713.5942D0,  4006.9160D0,  4316.6636D0,  4643.7568D0,
+           4989.1675D0,  5353.9209D0,  5739.1006D0,  6145.8501D0,  6575.3774D0,
+           7028.9585D0,  7507.9399D0,  8013.7441D0,  8547.8730D0,  9111.9131D0,
+           9707.5391D0, 10336.5205D0, 11000.7246D0, 11702.1240D0, 12442.8018D0,
+          13242.8018D0, 14042.8018D0, 14842.8018D0, 15642.8018D0, 16442.8008D0,
+          17242.8008D0, 18042.8008D0, 18842.8008D0, 19642.8008D0, 20442.8008D0,
+          21242.8008D0, 22042.8008D0, 22842.8008D0, 23642.8008D0, 24442.8008D0,
+          25242.8008D0, 26042.8008D0, 26842.8008D0, 27642.8008D0, 28442.8008D0,"
 
-"FZ(:) =    60.0000D0,   120.0000D0,   180.0000D0,   240.0000D0,   300.0000D0,
-           362.8800D0,   428.7783D0,   497.8396D0,   570.2159D0,   646.0663D0,
-           725.5576D0,   808.8643D0,   896.1698D0,   987.6660D0,  1083.5540D0,
-          1184.0446D0,  1289.3588D0,  1399.7280D0,  1515.3950D0,  1636.6140D0,
-          1763.6515D0,  1896.7867D0,  2036.3125D0,  2182.5354D0,  2335.7771D0,
-          2496.3745D0,  2664.6807D0,  2841.0654D0,  3025.9167D0,  3219.6409D0,
-          3422.6638D0,  3635.4319D0,  3858.4128D0,  4092.0969D0,  4336.9980D0,
-          4593.6543D0,  4862.6299D0,  5144.5161D0,  5439.9331D0,  5749.5303D0,
-          6073.9883D0,  6414.0205D0,  6770.3745D0,  7143.8335D0,  7535.2188D0,
-          7945.3906D0,  8375.2510D0,  8825.7451D0,  9297.8633D0,  9792.6436D0,
-         10292.6436D0, 10792.6436D0, 11292.6436D0, 11792.6436D0, 12292.6436D0,
-         12792.6436D0, 13292.6436D0, 13792.6436D0, 14292.6436D0, 14792.6436D0,
-         15292.6436D0, 15792.6436D0, 16292.6436D0, 16792.6445D0, 17292.6445D0,
-         17792.6445D0, 18292.6445D0, 18792.6445D0, 19292.6445D0, 19792.6445D0,
-         20292.6445D0, 20792.6445D0, 21292.6445D0, 21792.6445D0, 22292.6445D0,
-         22792.6445D0, 23292.6445D0, 23792.6445D0, 24292.6445D0, 24792.6445D0,"
+"FZ(:) =     60.0000D0,   120.0000D0,   180.0000D0,   240.0000D0,   300.0000D0,
+            362.8800D0,   428.7783D0,   497.8396D0,   570.2159D0,   646.0663D0,
+            725.5576D0,   808.8643D0,   896.1698D0,   987.6660D0,  1083.5540D0,
+           1184.0446D0,  1289.3588D0,  1399.7280D0,  1515.3950D0,  1636.6140D0,
+           1763.6515D0,  1896.7867D0,  2036.3125D0,  2182.5354D0,  2335.7771D0,
+           2496.3745D0,  2664.6807D0,  2841.0654D0,  3025.9167D0,  3219.6409D0,
+           3422.6638D0,  3635.4319D0,  3858.4128D0,  4092.0969D0,  4336.9980D0,
+           4593.6543D0,  4862.6299D0,  5144.5161D0,  5439.9331D0,  5749.5303D0,
+           6073.9883D0,  6414.0205D0,  6770.3745D0,  7143.8335D0,  7535.2188D0,
+           7945.3906D0,  8375.2510D0,  8825.7451D0,  9297.8633D0,  9792.6436D0,
+          10292.6436D0, 10792.6436D0, 11292.6436D0, 11792.6436D0, 12292.6436D0,
+          12792.6436D0, 13292.6436D0, 13792.6436D0, 14292.6436D0, 14792.6436D0,
+          15292.6436D0, 15792.6436D0, 16292.6436D0, 16792.6445D0, 17292.6445D0,
+          17792.6445D0, 18292.6445D0, 18792.6445D0, 19292.6445D0, 19792.6445D0,
+          20292.6445D0, 20792.6445D0, 21292.6445D0, 21792.6445D0, 22292.6445D0,
+          22792.6445D0, 23292.6445D0, 23792.6445D0, 24292.6445D0, 24792.6445D0,"
 ) # required num. of parameters for each domain
 
 BUFFER_DZ=(  "5000.0D0"  "5000.0D0" "5000.0D0" ) # required num. of parameters for each domain
@@ -114,18 +120,18 @@ ATMOS_PHY_RD_TYPE=( "MSTRNX"   "MSTRNX"   "MSTRNX"   ) # required num. of parame
 ATMOS_PHY_SF_TYPE=( "COUPLE"   "COUPLE"   "COUPLE"   ) # required num. of parameters for each domain
 ATMOS_PHY_TB_TYPE=( "MYNN"     "MYNN"     "MYNN"     ) # required num. of parameters for each domain
 
-OCEAN_TYPE=( "SLAB" "SLAB" "SLAB" ) # required num. of parameters for each domain
-LAND_TYPE=(  "SLAB" "SLAB" "SLAB" ) # required num. of parameters for each domain
-URBAN_TYPE=( "SLC"  "SLC"  "SLC"  ) # required num. of parameters for each domain
+OCEAN_TYPE=( "CONST" "CONST" "CONST" ) # required num. of parameters for each domain
+LAND_TYPE=(  "SLAB"  "SLAB"  "SLAB"  ) # required num. of parameters for each domain
+URBAN_TYPE=( "SLC"   "SLC"   "SLC"   ) # required num. of parameters for each domain
 
 # history parameters
 HIST_ITEMS_SNAPSHOT=(
 # ATMOS history
-  "DENS" "MOMZ" "MOMX" "MOMY"
+  "DENS" "MOMZ" "MOMX" "MOMY" "RHOT"
   "QV" "QC" "QR" "QI" "QS" "QG" "QHYD" "QLIQ" "QICE"
-  "T" "PRES" "U" "V" "W" "PT" "RH" "RHL" "RHI"
+  "T" "PRES" "U" "V" "W" "Uabs" "PT" "RH" "RHL" "RHI"
   "SFC_PRES" "SFC_TEMP" "SFC_Z0M"
-  "U10" "V10" "T2" "Q2" "LHFLX" "SHFLX" "GHFLX"
+  "U10" "V10" "Uabs10" "T2" "Q2" "MSLP" "LHFLX" "SHFLX" "GHFLX"
   "SFLX_LW_up" "SFLX_LW_dn" "SFLX_SW_up" "SFLX_SW_dn"
   "TOAFLX_LW_up" "TOAFLX_LW_dn" "TOAFLX_SW_up" "TOAFLX_SW_dn"
   "OSR" "OLR" "SLR" "SSR" "RADFLUX_SWUP" "RADFLUX_SWDN"
@@ -136,14 +142,18 @@ HIST_ITEMS_SNAPSHOT=(
 # URBAN history
   "URBAN_TC" "URBAN_SFC_TEMP"
 )
+HIST_ITEMS_SNAPSHOT_HI=(
+)
 HIST_ITEMS_AVERAGE=(
-  "PREC"
+)
+HIST_ITEMS_AVERAGE_HI=(
+  "RAIN" "SNOW" "PREC"
 )
 
 ##### for init
 BASENAME_ORG=""
 FILETYPE_ORG="NICAM-NETCDF"
-NUMBER_OF_FILES="8"
+NUMBER_OF_FILES=6
 WRF_FILE_TYPE=""
 
 INIT_USE_NESTING=".false."
@@ -154,18 +164,74 @@ PARENT_IMAX=""             # used if INIT_USE_NESTING = .true.
 PARENT_JMAX=""             # used if INIT_USE_NESTING = .true.
 PARENT_LKMAX=""            # used if INIT_USE_NESTING = .true.
 
+# linker
+LINK_BASEDIR="/data/share005/data/nicam/amip/trimmed_data"
+LINK_OUTPUTDIR="input"
+LINK_BOUNDARY_DT=86400
+LINK_FTYPE='peall'
+
 ##### for pp
 TOPOTYPE=(    "GTOPO30" "GTOPO30" "DEM50M" ) # required num. of parameters for each domain: GTOPO30, DEM50M
 LANDUSETYPE=( "GLCCv2"  "GLCCv2"  "LU100M" ) # required num. of parameters for each domain: GLCCv2, LU100M
 
 MAXSLOPE=(     "0.5D0" "1.5D0" "7.0D0" ) # required num. of parameters for each domain
-MAXSLOPE_BND=( "0.5D0" "0.5D0" "1.5D0" ) # required num. of parameters for each domain
+MAXSLOPE_BND=( "0.5D0" "1.5D0" "7.0D0" ) # required num. of parameters for each domain
 
 ##### for staging
 TRIMDIR=true
+#TRIMDIR=false
 
 INDIR="input"   # used if TRIMDIR = false
 OUTDIR="output" # used if TRIMDIR = false
+
+#################################################
+#
+# check parameters
+#
+#################################################
+
+if [ 7 -ne ${#STARTDATE[*]} ]; then echo "Error: Wrong array size (STARTDATE)."; exit 1; fi
+
+if [ ${NUM_DOMAIN} -ne ${#TIME_DT[*]} ];              then echo "Error: Wrong array size (TIME_DT).";              exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#TIME_DT_ATMOS_DYN[*]} ];    then echo "Error: Wrong array size (TIME_DT_ATMOS_DYN).";    exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#TIME_DT_ATMOS_PHY_MP[*]} ]; then echo "Error: Wrong array size (TIME_DT_ATMOS_PHY_MP)."; exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#TIME_DT_ATMOS_PHY_RD[*]} ]; then echo "Error: Wrong array size (TIME_DT_ATMOS_PHY_RD)."; exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#TIME_DT_ATMOS_PHY_SF[*]} ]; then echo "Error: Wrong array size (TIME_DT_ATMOS_PHY_SF)."; exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#TIME_DT_ATMOS_PHY_TB[*]} ]; then echo "Error: Wrong array size (TIME_DT_ATMOS_PHY_TB)."; exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#TIME_DT_OCEAN[*]} ];        then echo "Error: Wrong array size (TIME_DT_OCEAN).";        exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#TIME_DT_LAND[*]} ];         then echo "Error: Wrong array size (TIME_DT_LAND).";         exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#TIME_DT_URBAN[*]} ];        then echo "Error: Wrong array size (TIME_DT_URBAN).";        exit 1; fi
+
+if [ ${NUM_DOMAIN} -ne ${#PRC_NUM_X[*]} ]; then echo "Error: Wrong array size (PRC_NUM_X)."; exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#PRC_NUM_Y[*]} ]; then echo "Error: Wrong array size (PRC_NUM_Y)."; exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#KMAX[*]} ];      then echo "Error: Wrong array size (KMAX).";      exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#IMAX[*]} ];      then echo "Error: Wrong array size (IMAX).";      exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#JMAX[*]} ];      then echo "Error: Wrong array size (JMAX).";      exit 1; fi
+ 
+if [ ${LKMAX} -ne ${#LDZ[*]} ]; then echo "Error: Wrong array size (LDZ)."; exit 1; fi
+if [ ${UKMAX} -ne ${#UDZ[*]} ]; then echo "Error: Wrong array size (UDZ)."; exit 1; fi
+
+if [ ${NUM_DOMAIN} -ne ${#DX[*]} ];    then echo "Error: Wrong array size (DX).";    exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#DY[*]} ];    then echo "Error: Wrong array size (DY).";    exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#DEF_Z[*]} ]; then echo "Error: Wrong array size (DEF_Z)."; exit 1; fi
+
+if [ ${NUM_DOMAIN} -ne ${#BUFFER_DZ[*]} ]; then echo "Error: Wrong array size (BUFFER_DZ)."; exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#BUFFER_DX[*]} ]; then echo "Error: Wrong array size (BUFFER_DX)."; exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#BUFFER_DY[*]} ]; then echo "Error: Wrong array size (BUFFER_DY)."; exit 1; fi
+
+if [ ${NUM_DOMAIN} -ne ${#ATMOS_DYN_TYPE[*]} ];    then echo "Error: Wrong array size (ATMOS_DYN_TYPE).";    exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#ATMOS_PHY_MP_TYPE[*]} ]; then echo "Error: Wrong array size (ATMOS_PHY_MP_TYPE)."; exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#ATMOS_PHY_RD_TYPE[*]} ]; then echo "Error: Wrong array size (ATMOS_PHY_RD_TYPE)."; exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#ATMOS_PHY_SF_TYPE[*]} ]; then echo "Error: Wrong array size (ATMOS_PHY_SF_TYPE)."; exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#ATMOS_PHY_TB_TYPE[*]} ]; then echo "Error: Wrong array size (ATMOS_PHY_TB_TYPE)."; exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#OCEAN_TYPE[*]} ];        then echo "Error: Wrong array size (OCEAN_TYPE).";        exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#LAND_TYPE[*]} ];         then echo "Error: Wrong array size (LAND_TYPE).";         exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#URBAN_TYPE[*]} ];        then echo "Error: Wrong array size (URBAN_TYPE).";        exit 1; fi
+
+if [ ${NUM_DOMAIN} -ne ${#TOPOTYPE[*]} ];     then echo "Error: Wrong array size (TOPOTYPE).";     exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#LANDUSETYPE[*]} ];  then echo "Error: Wrong array size (LANDUSETYPE).";  exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#MAXSLOPE[*]} ];     then echo "Error: Wrong array size (MAXSLOPE).";     exit 1; fi
+if [ ${NUM_DOMAIN} -ne ${#MAXSLOPE_BND[*]} ]; then echo "Error: Wrong array size (MAXSLOPE_BND)."; exit 1; fi
 
 #################################################
 #
@@ -179,6 +245,7 @@ RUN_CONF="conf/base.run.conf.sh"
 PARAM_CONF="conf/param.conf.sh"
 LAUNCH_CONF="conf/base.launch.conf.sh"
 JOB_SHELL="conf/base.jobshell.sh"
+LINK_SHELL="conf/base.linkinput.nicam.sh"
 
 LANDPARAMDIR="data"
 LAND_PROPERTY_IN_FILENAME="${LANDPARAMDIR}/param.bucket.conf"
@@ -270,6 +337,7 @@ TOPO_OUT_BASENAME="${TOPO_IN_BASENAME}"
 LANDUSE_OUT_BASENAME="${LANDUSE_IN_BASENAME}"
 RUN_RESTART_IN_BASENAME="${INIT_RESTART_OUT_BASENAME}_${INITSEC}"
 ATMOS_BOUNDARY_IN_BASENAME="${BASENAME_BOUNDARY}"
+ATMOS_BOUNDARY_START_DATE="${TIME_STARTDATE}"
 
 # set nesting parameters
 if [ $DNUM -lt $NUM_DOMAIN ]; then
@@ -285,12 +353,15 @@ fi
 
 # set boundary parameters
 if [ $DNUM -gt 1 ]; then
-  ATMOS_BOUNDARY_USE_QHYD=".true."
   ATMOS_BOUNDARY_IN_BASENAME=""
+  ATMOS_BOUNDARY_START_DATE=""
   ATMOS_BOUNDARY_UPDATE_DT="0.D0"
+  ATMOS_BOUNDARY_USE_DENS=".false."
+  ATMOS_BOUNDARY_USE_QHYD=".true."
 else
-  ATMOS_BOUNDARY_USE_QHYD=".false."
   ATMOS_BOUNDARY_UPDATE_DT="${TIME_DT_BOUNDARY}"
+  ATMOS_BOUNDARY_USE_DENS=".true."
+  ATMOS_BOUNDARY_USE_QHYD=".false."
 fi
 
 #################################################
@@ -360,5 +431,14 @@ fi
 
 source ${JOB_SHELL}
 mv -f base.jobshell run.nest.sh
+
+#################################################
+#
+# make initial link shell
+#
+#################################################
+
+source ${LINK_SHELL}
+mv -f base.linkinput linkinput.sh
 
 #################################################
