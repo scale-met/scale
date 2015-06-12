9d1191cc2ac504179509218f3cfa465435a9909d
Seiya Nishizawa <seiya@gfd-dennou.org>
2015-06-12 15:34:44 +0900

    fixed bug in the previous commit

--- scalelib/src/atmos-les/dynamics/scale_atmos_dyn_rk_hevi.F90
+++ scalelib/src/atmos-les/dynamics/scale_atmos_dyn_rk_hevi.F90
@@ -1648,20 +1648,18 @@ contains
   subroutine mul_matrix(V, M, C)
     implicit none
     real(RP), intent(out) :: V(KMAX-1)
-    real(RP), intent(in)  :: M(5, KMAX-1)
+    real(RP), intent(in)  :: M(3, KMAX-1)
     real(RP), intent(in)  :: C(KMAX-1)
 
     integer :: k
 
-    V(1) = M(5,1)*C(3) + M(4,1)*C(2) + M(3,1)*C(1)
-    V(2) = M(5,2)*C(4) + M(4,2)*C(3) + M(3,2)*C(2) + M(2,2)*C(1)
-    do k = 3, KMAX-3
-       V(k) = M(5,k)*C(k+2) + M(4,k)*C(k+1) + M(3,k)*C(k) + M(2,k)*C(k-1) + M(1,k)*C(k-2)
+    ! k = 1
+    V(1) = M(3,1)*C(2) + M(2,1)*C(1)
+    do k = 2, KMAX-2
+       V(k) = M(3,k)*C(k+1) + M(2,k)*C(k) + M(1,k)*C(k-1)
     enddo
-    ! k = KE-2
-    V(KMAX-2) = M(4,KMAX-2)*C(KMAX-1) + M(3,KMAX-2)*C(KMAX-2) + M(2,KMAX-2)*C(KMAX-3) + M(1,KMAX-2)*C(KMAX-4)
     ! k = KE-1
-    V(KMAX-1) = M(3,KMAX-1)*C(KMAX-1) + M(2,KMAX-1)*C(KMAX-2) + M(1,KMAX-1)*C(KMAX-3)
+    V(KMAX-1) = M(2,KMAX-1)*C(KMAX-1) + M(1,KMAX-1)*C(KMAX-2)
 
     return
   end subroutine mul_matrix
