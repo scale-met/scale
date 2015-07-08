16399da0b863e792a36c711c01083e2662044442
Ryuji Yoshida <ryoshida@riken.jp>
2015-07-08 15:13:59 +0900

    modified directory structure of tutorial and usersguide.

---
+++ scale-les/test/tutorial/doc/usersguide_make.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#-----------------------------------------
+#   Test Script for XXX  (Ver.0.1)
+#   2009/10/26 --- Ryuji Yoshida.
+#-----------------------------------------
+# first time
+platex scale_users_guide.tex
+# second time
+platex scale_users_guide.tex
+# dvi to pdf
+dvipdfmx scale_users_guide.dvi
+#
+acroread scale_users_guide.pdf
+#
+#-----------------------------------------
+#EOF
