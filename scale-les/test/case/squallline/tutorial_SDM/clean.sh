34d97915cc7a3f948267630ed831aef345ce72ea
Shin-ichiro Shima <s_shima@sim.u-hyogo.ac.jp>
2016-02-13 17:41:19 +0900

    [add] Squall line case for SDM is added modifying the tutorial

---
+++ scale-les/test/case/squallline/tutorial_SDM/clean.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -x 
+rm -f *.log *.nc init_LOG.pe000000 LOG.pe000000 monitor.pe000000 output_run.log random_number_init.* SD_output_* superdroplet_restart_* random_number_restart_* *.sh.[ios]* *[23]d.ctl *[23]d.grd
