344224b1af98a540115167d70bc1e0521c7f2e22
Hisashi Yashiro <h.yashiro@riken.jp>
2016-03-04 11:53:40 +0900

    Modify in Tomita08 : Add more limiter when v->i

--- scalelib/src/atmos-physics/microphysics/scale_atmos_phy_mp_tomita08.F90
+++ scalelib/src/atmos-physics/microphysics/scale_atmos_phy_mp_tomita08.F90
@@ -1504,12 +1504,18 @@ contains
 
           ! update Qv, Qi with limiter
           dq   = ( Pigen + Pidep ) * dt
-          qtmp = QTRC0(k,i,j,I_QV) - dq
-          qtmp = min( qtmp, QSATI(k,i,j) )
-          dq   = QTRC0(k,i,j,I_QV) - qtmp
-          qtmp = QTRC0(k,i,j,I_QI) + dq
-          qtmp = max( qtmp, 0.D0 )
-          dq   = qtmp - QTRC0(k,i,j,I_QI)
+          if ( dq > 0.0_RP ) then ! v->i
+             qtmp = QTRC0(k,i,j,I_QV) - dq
+             qtmp = max( qtmp, QSATI(k,i,j) )
+             dq   = QTRC0(k,i,j,I_QV) - qtmp
+          else                    ! i->v
+             qtmp = QTRC0(k,i,j,I_QV) - dq
+             qtmp = min( qtmp, QSATI(k,i,j) )
+             dq   = QTRC0(k,i,j,I_QV) - qtmp
+             qtmp = QTRC0(k,i,j,I_QI) + dq
+             qtmp = max( qtmp, 0.0_RP )
+             dq   = qtmp - QTRC0(k,i,j,I_QI)
+          endif
           QTRC0 (k,i,j,I_QI) = QTRC0 (k,i,j,I_QI) + dq
           QTRC0 (k,i,j,I_QV) = QTRC0 (k,i,j,I_QV) - dq
           QTRC_t(k,i,j,I_QI) = QTRC_t(k,i,j,I_QI) + dq /dt
