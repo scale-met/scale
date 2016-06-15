73e0a07e76535ab2e3bc413d6c60be47f2702f37
Sachiho Adachi <sachiho.adachi@riken.jp>
2016-06-15 21:04:42 +0900

    update parts of descriptions for V5.0.0

--- scale-rm/test/tutorial/doc/usersguide_make.sh
+++ scale-rm/test/tutorial/doc/usersguide_make.sh
@@ -2,9 +2,18 @@
 #-----------------------------------------
 #   Test Script for XXX  (Ver.0.1)
 #   2009/10/26 --- Ryuji Yoshida.
+#   2016/06/15  modified  S.Adachi
 #-----------------------------------------
 #
 FNAME="scale_users_guide"
+#
+#---- Replace Version
+scale_version=`gawk '{print $0}' ../../../src/VERSION`
+#echo "SCALE VERSION" $scale_version
+#sed 3s/_VERSION_/$scale_version/ template/scale_users_guide.tex > scale_users_guide.tex
+#sed 4s/_VERSION_/$scale_version/ template/11_introduction.tex   > 11_introduction.tex
+#
+#
 #
 # first time
 platex ${FNAME}.tex
