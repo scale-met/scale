c7f48425a3e530294b3c87ad5b9300fa2bfb5222
Hisashi Yashiro <h.yashiro@riken.jp>
2016-09-23 16:53:38 +0900

    1) Merge Makefile.pp.common in framework and case_real, 2) Merge Makejobshell.pp.*  in framework and case_real, 3) Add Makefile.tutorial.common

---
+++ scale-rm/test/sysdep/Mkjobshell.net2g.MacOSX-gnu-ompi.sh
@@ -0,0 +1,26 @@
+#! /bin/bash -x
+
+# Arguments
+BINDIR=${1}
+NET2GCONF=${2}
+TPROC=${3}
+
+# System specific
+MPIEXEC="mpirun -np ${TPROC}"
+
+# Generate run.sh
+
+cat << EOF1 > ./net2g.sh
+#! /bin/bash -x
+################################################################################
+#
+# ------ For MacOSX & GNU fortran&C & openmpi -----
+#
+################################################################################
+export FORT_FMT_RECL=400
+export GFORTRAN_UNBUFFERED_ALL=Y
+
+# run
+${MPIEXEC} ${BINDIR}/net2g ${NET2GCONF} || exit 1
+
+EOF1
