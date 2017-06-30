9bcc5482ffc89a6ebe9ed56cc476657c5694132d
Hisashi Yashiro <h.yashiro@riken.jp>
2017-06-30 17:09:44 +0900

    Modify restart test for other machines, remove unused test

---
+++ scale-rm/test/case/restart/Mkjobshell.Linux64-intel-impi.sh
@@ -0,0 +1,112 @@
+#! /bin/bash -x
+
+# Arguments
+BINDIR=${1}
+PPNAME=${2}
+INITNAME=${3}
+BINNAME=${4}
+PPCONF=${5}
+INITCONF=${6}
+RUNCONF=${7}
+TPROC=${8}
+eval DATPARAM=(`echo ${9}  | tr -s '[' '"' | tr -s ']' '"'`)
+eval DATDISTS=(`echo ${10} | tr -s '[' '"' | tr -s ']' '"'`)
+
+# System specific
+MPIEXEC="mpirun -np ${TPROC}"
+
+if [ ! ${PPCONF} = "NONE" ]; then
+  RUN_PP="${MPIEXEC} ${BINDIR}/${PPNAME} ${PPCONF} || exit"
+fi
+
+if [ ! ${INITCONF} = "NONE" ]; then
+  RUN_INIT="${MPIEXEC} ${BINDIR}/${INITNAME} ${INITCONF} || exit"
+fi
+
+if [ ! ${RUNCONF} = "NONE" ]; then
+  RUN_BIN="${MPIEXEC} ${BINDIR}/${BINNAME} ${RUNCONF} || exit"
+fi
+
+
+
+
+
+cat << EOF1 > ./run.sh
+#! /bin/bash -x
+################################################################################
+#
+# ------ For Linux64 & intel fortran&C & intel mpi -----
+#
+################################################################################
+export FORT_FMT_RECL=400
+
+
+EOF1
+
+# link to file or directory
+ndata=${#DATPARAM[@]}
+
+if [ ${ndata} -gt 0 ]; then
+   for n in `seq 1 ${ndata}`
+   do
+      let i="n - 1"
+
+      pair=(${DATPARAM[$i]})
+
+      src=${pair[0]}
+      dst=${pair[1]}
+      if [ "${dst}" = "" ]; then
+         dst=${pair[0]}
+      fi
+
+      if [ -f ${src} ]; then
+         echo "ln -svf ${src} ./${dst}" >> ./run.sh
+      elif [ -d ${src} ]; then
+         echo "rm -f          ./${dst}" >> ./run.sh
+         echo "ln -svf ${src} ./${dst}" >> ./run.sh
+      else
+         echo "datafile does not found! : ${src}"
+         exit 1
+      fi
+   done
+fi
+
+# link to distributed file
+ndata=${#DATDISTS[@]}
+
+if [ ${ndata} -gt 0 ]; then
+   for n in `seq 1 ${ndata}`
+   do
+      let i="n - 1"
+
+      pair=(${DATDISTS[$i]})
+
+      for np in `seq 1 ${TPROC}`
+      do
+         let "ip = ${np} - 1"
+         PE=`printf %06d ${ip}`
+
+         src=${pair[0]}.pe${PE}.nc
+         dst=${pair[1]}.pe${PE}.nc
+
+         if [ -f ${src} ]; then
+            echo "ln -svf ${src} ./${dst}" >> ./run.sh
+         else
+            echo "datafile does not found! : ${src}"
+            exit 1
+         fi
+      done
+   done
+fi
+
+cat << EOF2 >> ./run.sh
+
+# run
+${RUN_PP}
+${RUN_INIT}
+${MPIEXEC} ${BINDIR}/${BINNAME} runA.conf  || exit
+${MPIEXEC} ${BINDIR}/${BINNAME} runB1.conf || exit
+${MPIEXEC} ${BINDIR}/${BINNAME} runB2.conf || exit
+
+################################################################################
+EOF2
