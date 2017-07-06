c08f055dbea2da82f8731fd7c89d5b8e6d8eb03f
Hisashi Yashiro <h.yashiro@riken.jp>
2017-07-07 00:27:41 +0900

    Update visualization script in realcase

--- scale-rm/test/realcase/masscheck/run_FNL/visualize/visualize.sh
+++ scale-rm/test/realcase/masscheck/run_FNL/visualize/visualize.sh
@@ -1,12 +1,12 @@
 #! /bin/bash -x
 
-for domain in d01 d02
-do
-
 ### Energy & Mass balance ###
 
 echo "+visualize by gnuplot"
 rm -f energy.dat energy_flx.dat mass.dat mass_q.dat
+
+for domain in d01 d02
+do
 
 while read -a line
 do
@@ -29,15 +29,18 @@ mv Energy_FLX.png Energy_FLX_${domain}.png
 mv Mass.png       Mass_${domain}.png
 mv Mass_q.png     Mass_q_${domain}.png
 
+done
+
 
 
 ### Visalization ###
 echo "+visualize by gpview"
 rm -f dcl.pdf
 
+for domain in d01 d02
+do
 
-### Land Use ###
-var__set=(FRAC_LAND FRAC_URBAN)
+var__set=(PRES T    U    V    W    QV   QHYD)
 rangeset=(auto auto auto auto auto auto auto)
 
 i=0
@@ -49,17 +52,16 @@ do
       range="--range="${rangeset[$i]}
    fi
 
-   # time series
-   gpview landuse_${domain}.pe\*.nc@${var} --aspect 1 --nocont --wsn 2 || exit
-   convert -density 150 -rotate 90 +antialias dcl.pdf LANDUSE.${var}_${domain}.png
+   # average
+   gpview history_${domain}.pe\*.nc@${var},z=0 --nocont --aspect 1 --mean time --wsn 2 || exit
+   convert -density 150 -rotate 90 +antialias dcl.pdf hist.${var}_${domain}.png
    rm -f dcl.pdf
 
    let i="${i} + 1"
 done
 
-### Topography ###
-var__set=(TOPO)
-rangeset=(auto)
+var__set=(U10  V10  T2   Q2   SFC_PRES SFC_TEMP PREC LHFLX SHFLX GHFLX SFLX_LW_dn SFLX_SW_dn)
+rangeset=(auto auto auto auto auto     auto     auto auto  auto  auto  auto       auto      )
 
 i=0
 for var in ${var__set[@]}
@@ -70,98 +72,9 @@ do
       range="--range="${rangeset[$i]}
    fi
 
-   # time series
-   gpview topo_${domain}.pe\*.nc@${var} --aspect 1 --nocont --wsn 2 || exit
-   convert -density 150 -rotate 90 +antialias dcl.pdf ${var}_${domain}.png
-   rm -f dcl.pdf
-
-   let i="${i} + 1"
-done
-
-### Initial ###
-var__set=(DENS MOMZ MOMX MOMY RHOT QV)
-rangeset=(auto auto auto auto auto auto)
-
-i=0
-for var in ${var__set[@]}
-do
-   if [ ${rangeset[$i]} == "auto" ]; then
-      range=""
-   else
-      range="--range="${rangeset[$i]}
-   fi
-
-   if [ ${var} == "MOMZ" ]; then
-      level="zh"
-   else
-      level="z"
-   fi
-
-   # time series
-   gpview init_${domain}_*.pe\*.nc@${var},${level}=0 --aspect 1 --nocont --wsn 2 || exit
-   convert -density 150 -rotate 90 +antialias dcl.pdf INITIAIL.${var}_${domain}.png
-   rm -f dcl.pdf
-
-   let i="${i} + 1"
-done
-
-### Boundary ###
-var__set=(DENS VELZ VELX VELY POTT QV)
-rangeset=(auto auto auto auto auto auto)
-
-i=0
-for var in ${var__set[@]}
-do
-   if [ ${rangeset[$i]} == "auto" ]; then
-      range=""
-   else
-      range="--range="${rangeset[$i]}
-   fi
-
-   # time series
-   gpview boundary_${domain}.pe\*.nc@${var},z=0 --mean time --aspect 1 --nocont --wsn 2 || exit
-   convert -density 150 -rotate 90 +antialias dcl.pdf BOUNDARY.${var}_${domain}.png
-   rm -f dcl.pdf
-
-   let i="${i} + 1"
-done
-
-### History ###
-var__set=(DENS PT U V W QV QHYD)
-rangeset=(auto auto auto auto auto auto auto auto)
-
-i=0
-for var in ${var__set[@]}
-do
-   if [ ${rangeset[$i]} == "auto" ]; then
-      range=""
-   else
-      range="--range="${rangeset[$i]}
-   fi
-
-   # time series
-   gpview history_${domain}.pe\*.nc@${var},z=0 --mean time --aspect 1 --nocont --wsn 2 || exit
-   convert -density 150 -rotate 90 +antialias dcl.pdf HISTORY.${var}_${domain}.png
-   rm -f dcl.pdf
-
-   let i="${i} + 1"
-done
-
-var__set=(U10 V10 T2 Q2 SFC_PRES SFC_TEMP PREC LHFLX SHFLX GHFLX SFLX_LW_dn SFLX_SW_dn)
-rangeset=(auto auto auto auto auto auto auto auto auto auto auto)
-
-i=0
-for var in ${var__set[@]}
-do
-   if [ ${rangeset[$i]} == "auto" ]; then
-      range=""
-   else
-      range="--range="${rangeset[$i]}
-   fi
-
-   # time series
-   gpview history_${domain}.pe\*.nc@${var} --mean time --aspect 1 --nocont --wsn 2 || exit
-   convert -density 150 -rotate 90 +antialias dcl.pdf HISTORY.${var}_${domain}.png
+   # average
+   gpview history_${domain}.pe\*.nc@${var} --nocont --aspect 1 --mean time --wsn 2 || exit
+   convert -density 150 -rotate 90 +antialias dcl.pdf hist.${var}_${domain}.png
    rm -f dcl.pdf
 
    let i="${i} + 1"
