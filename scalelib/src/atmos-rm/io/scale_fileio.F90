scale-met/scale

commit 131c166d2888c348e1cda5f0ce877022ff4ea6fe

add the bottom data of half level variables to files by FILEIO

scalelib/src/atmos-rm/io/scale_fileio.F90
@@ -213,8 +213,8 @@ contains
     allocate( AXIS_LATY (IM,JM) )
     allocate( AXIS_LATXY(IM,JM) )
 
-    allocate( AXIS_HZXY (KMAX,IM,JM) )
-    allocate( AXIS_HWXY (KMAX,IM,JM) )
+    allocate( AXIS_HZXY (KMAX  ,IM,JM) )
+    allocate( AXIS_HWXY (KMAX+1,IM,JM) )
 
     if( IO_AGGREGATE ) call Construct_Derived_Datatype
 
@@ -285,8 +285,8 @@ contains
     AXIS_LATY (:,:)   = LATY (XSB:XEB,YSB:YEB) / D2R
     AXIS_LATXY(:,:)   = LATXY(XSB:XEB,YSB:YEB) / D2R
 
-    AXIS_HZXY (:,:,:) = CZ(KS:KE,XSB:XEB,YSB:YEB)
-    AXIS_HWXY (:,:,:) = FZ(KS:KE,XSB:XEB,YSB:YEB)
+    AXIS_HZXY (:,:,:) = CZ(KS  :KE,XSB:XEB,YSB:YEB)
+    AXIS_HWXY (:,:,:) = FZ(KS-1:KE,XSB:XEB,YSB:YEB)
 
     set_coordinates = .true.
 
@@ -907,8 +907,17 @@ contains
           call PRC_MPIstop
        endif
     else
-       if    ( axistype == 'ZXY' ) then
+       if(      axistype == 'ZXY'  &
+           .or. axistype == 'ZXHY' &
+           .or. axistype == 'ZXYH' ) then
           dim1_S   = KS
+          dim1_E   = KE
+          dim2_S   = ISB
+          dim2_E   = IEB
+          dim3_S   = JSB
+          dim3_E   = JEB
+       elseif( axistype == 'ZHXY' ) then
+          dim1_S   = KS-1
           dim1_E   = KE
           dim2_S   = ISB
           dim2_E   = IEB
@@ -994,8 +1003,19 @@ contains
           call PRC_MPIstop
        endif
     else
-       if ( axistype == 'ZXYT' ) then
+       if (      axistype == 'ZXYT'  &
+            .or. axistype == 'ZXHYT' &
+            .or. axistype == 'ZXYHT' ) then
           dim1_S   = KS
+          dim1_E   = KE
+          dim2_S   = ISB
+          dim2_E   = IEB
+          dim3_S   = JSB
+          dim3_E   = JEB
+          dim4_S   = 1
+          dim4_E   = step
+       elseif ( axistype == 'ZHXYT' ) then
+          dim1_S   = KS-1
           dim1_E   = KE
           dim2_S   = ISB
           dim2_E   = IEB
@@ -1641,7 +1661,7 @@ contains
     end if
 
     if ( .NOT. xy_ ) then
-       call FileDefAxis( fid, 'zh',  'Z (half level)',  'm', 'zh',  dtype, KMAX )
+       call FileDefAxis( fid, 'zh',  'Z (half level)',  'm', 'zh',  dtype, KMAX+1 )
     end if
     if ( .NOT. IO_AGGREGATE ) then
        call FileDefAxis( fid, 'xh',  'X (half level)',  'm', 'xh',  dtype, IMAXB )
@@ -1652,10 +1672,10 @@ contains
     end if
 
     if ( .NOT. xy_ ) then
-       call FileDefAxis( fid, 'lz',  'LZ',              'm', 'lz',  dtype, LKMAX )
-       call FileDefAxis( fid, 'lzh', 'LZ (half level)', 'm', 'lzh', dtype, LKMAX )
-       call FileDefAxis( fid, 'uz',  'UZ',              'm', 'uz',  dtype, UKMAX )
-       call FileDefAxis( fid, 'uzh', 'UZ (half level)', 'm', 'uzh', dtype, UKMAX )
+       call FileDefAxis( fid, 'lz',  'LZ',              'm', 'lz',  dtype, LKMAX   )
+       call FileDefAxis( fid, 'lzh', 'LZ (half level)', 'm', 'lzh', dtype, LKMAX+1 )
+       call FileDefAxis( fid, 'uz',  'UZ',              'm', 'uz',  dtype, UKMAX   )
+       call FileDefAxis( fid, 'uzh', 'UZ (half level)', 'm', 'uzh', dtype, UKMAX+1 )
     end if
 
     if ( .NOT. xy_ ) then
@@ -2007,12 +2027,12 @@ contains
 
     if ( put_z ) then
        if( IO_AGGREGATE ) start(1) = 1
-       call FileWriteAxis( fid, 'z',   GRID_CZ(KS:KE),    start )
-       call FileWriteAxis( fid, 'zh',  GRID_FZ(KS:KE),    start )
-       call FileWriteAxis( fid, 'lz',  GRID_LCZ(LKS:LKE), start )
-       call FileWriteAxis( fid, 'lzh', GRID_LFZ(LKS:LKE), start )
-       call FileWriteAxis( fid, 'uz',  GRID_UCZ(UKS:UKE), start )
-       call FileWriteAxis( fid, 'uzh', GRID_UFZ(UKS:UKE), start )
+       call FileWriteAxis( fid, 'z',   GRID_CZ (KS   :KE),  start )
+       call FileWriteAxis( fid, 'zh',  GRID_FZ (KS-1 :KE),  start )
+       call FileWriteAxis( fid, 'lz',  GRID_LCZ(LKS  :LKE), start )
+       call FileWriteAxis( fid, 'lzh', GRID_LFZ(LKS-1:LKE), start )
+       call FileWriteAxis( fid, 'uz',  GRID_UCZ(UKS  :UKE), start )
+       call FileWriteAxis( fid, 'uzh', GRID_UFZ(UKS-1:UKE), start )
     end if
     if ( put_x ) then
        if( IO_AGGREGATE ) start(1) = ISGA
@@ -2242,7 +2262,7 @@ contains
     elseif( axistype == 'ZHXY' ) then
        ndims   = 3
        dims    = (/'zh','x ','y '/)
-       write_buf_amount = write_buf_amount + KA * IA * JA * elm_size
+       write_buf_amount = write_buf_amount + (KA+1) * IA * JA * elm_size
        call MPRJ_get_attributes( mapping )
     elseif( axistype == 'ZXHY' ) then
        ndims   = 3
@@ -2563,11 +2583,14 @@ contains
     end if
 
     if (      axistype == 'ZXY'  &
-         .OR. axistype == 'ZHXY' &
          .OR. axistype == 'ZXHY' &
          .OR. axistype == 'ZXYH' ) then
        dim1_max = KMAX
        dim1_S   = KS
+       dim1_E   = KE
+    elseif ( axistype == 'ZHXY' ) then
+       dim1_max = KMAX+1
+       dim1_S   = KS-1
        dim1_E   = KE
     elseif( axistype == 'Land' ) then
        dim1_max = LKMAX

scale-rm/src/atmos/mod_atmos_vars.f90
@@ -940,15 +940,15 @@ contains
        if( IO_L ) write(IO_FID_LOG,*) '*** Read from restart file (ATMOS) ***'
 
        call FILEIO_read( DENS(:,:,:),                                     & ! [OUT]
-                         restart_fid, PV_info(I_DENS)%NAME, 'ZXY', step=1 ) ! [IN]
+                         restart_fid, PV_info(I_DENS)%NAME, 'ZXY',  step=1 ) ! [IN]
        call FILEIO_read( MOMZ(:,:,:),                                     & ! [OUT]
-                         restart_fid, PV_info(I_MOMZ)%NAME, 'ZXY', step=1 ) ! [IN]
+                         restart_fid, PV_info(I_MOMZ)%NAME, 'ZHXY', step=1 ) ! [IN]
        call FILEIO_read( MOMX(:,:,:),                                     & ! [OUT]
-                         restart_fid, PV_info(I_MOMX)%NAME, 'ZXY', step=1 ) ! [IN]
+                         restart_fid, PV_info(I_MOMX)%NAME, 'ZXHY', step=1 ) ! [IN]
        call FILEIO_read( MOMY(:,:,:),                                     & ! [OUT]
-                         restart_fid, PV_info(I_MOMY)%NAME, 'ZXY', step=1 ) ! [IN]
+                         restart_fid, PV_info(I_MOMY)%NAME, 'ZXYH', step=1 ) ! [IN]
        call FILEIO_read( RHOT(:,:,:),                                     & ! [OUT]
-                         restart_fid, PV_info(I_RHOT)%NAME, 'ZXY', step=1 ) ! [IN]
+                         restart_fid, PV_info(I_RHOT)%NAME, 'ZXY',  step=1 ) ! [IN]
 
        do iq = 1, QA
           call FILEIO_read( QTRC(:,:,:,iq),                             & ! [OUT]
@@ -962,7 +962,7 @@ contains
           do j  = 1, JA
           do i  = 1, IA
              DENS(   1:KS-1,i,j) = DENS(KS,i,j)
-             MOMZ(   1:KS-1,i,j) = MOMZ(KS,i,j)
+             MOMZ(   1:KS-2,i,j) = MOMZ(KS-1,i,j)
              MOMX(   1:KS-1,i,j) = MOMX(KS,i,j)
              MOMY(   1:KS-1,i,j) = MOMY(KS,i,j)
              RHOT(   1:KS-1,i,j) = RHOT(KS,i,j)
@@ -1072,11 +1072,11 @@ contains
 
     call FILEIO_open( fid, basename )
 
-    call FILEIO_read( DENS_check(:,:,:), fid, 'DENS', 'ZXY', step=1 )
-    call FILEIO_read( MOMZ_check(:,:,:), fid, 'MOMZ', 'ZXY', step=1 )
-    call FILEIO_read( MOMX_check(:,:,:), fid, 'MOMX', 'ZXY', step=1 )
-    call FILEIO_read( MOMY_check(:,:,:), fid, 'MOMY', 'ZXY', step=1 )
-    call FILEIO_read( RHOT_check(:,:,:), fid, 'RHOT', 'ZXY', step=1 )
+    call FILEIO_read( DENS_check(:,:,:), fid, 'DENS', 'ZXY' , step=1 )
+    call FILEIO_read( MOMZ_check(:,:,:), fid, 'MOMZ', 'ZHXY', step=1 )
+    call FILEIO_read( MOMX_check(:,:,:), fid, 'MOMX', 'ZXHY', step=1 )
+    call FILEIO_read( MOMY_check(:,:,:), fid, 'MOMY', 'ZXYH', step=1 )
+    call FILEIO_read( RHOT_check(:,:,:), fid, 'RHOT', 'ZXY' , step=1 )
     do iq = 1, QA
        call FILEIO_read( QTRC_check(:,:,:,iq), fid, TRACER_NAME(iq), 'ZXY', step=1 )
     end do
@@ -1096,7 +1096,7 @@ contains
     enddo
     enddo
 
-    do k = KS, KE
+    do k = KS-1, KE
     do j = JS, JE
     do i = IS, IE
        if ( abs( MOMZ(k,i,j)-MOMZ_check(k,i,j) ) > ATMOS_RESTART_CHECK_CRITERION ) then

commit 6dee027628481b81928021cb21568b8de75f668f

fixed the bug introduced by 131c166d

scalelib/src/atmos-rm/io/scale_fileio.F90
@@ -139,6 +139,7 @@ module scale_fileio
   integer,  private              :: startXY   (3), countXY   (3)
   integer,  private              :: startZX   (2), countZX   (2)
   integer,  private              :: startZXY  (4), countZXY  (4)
+  integer,  private              :: startZHXY (4), countZHXY (4)
   integer,  private              :: startLAND (3), countLAND (3)
   integer,  private              :: startURBAN(3), countURBAN(3)
   ! local start and end
@@ -151,6 +152,7 @@ module scale_fileio
   integer,  private              :: centerTypeXY
   integer,  private              :: centerTypeZX
   integer,  private              :: centerTypeZXY
+  integer,  private              :: centerTypeZHXY
   integer,  private              :: centerTypeLAND
   integer,  private              :: centerTypeURBAN
 
@@ -470,6 +472,7 @@ contains
     centerTypeXY    = MPI_DATATYPE_NULL
     centerTypeZX    = MPI_DATATYPE_NULL
     centerTypeZXY   = MPI_DATATYPE_NULL
+    centerTypeZHXY  = MPI_DATATYPE_NULL
     centerTypeLAND  = MPI_DATATYPE_NULL
     centerTypeURBAN = MPI_DATATYPE_NULL
 
@@ -483,10 +486,10 @@ contains
     countXY(1)    = IA
     countXY(2)    = JA
     ! for axistype == 'ZXY'
-    startZXY(1)   = 1
-    startZXY(2:3) = startXY(1:2)
-    countZXY(1)   = KMAX
-    countZXY(2:3) = countXY(1:2)
+    startZXY(1)    = 1
+    startZXY(2:3)  = startXY(1:2)
+    countZXY(1)    = KMAX
+    countZXY(2:3)  = countXY(1:2)
     ! construct MPI subarray data type
     sizes(1)      = KA
     sizes(2)      = IA
@@ -497,9 +500,27 @@ contains
     sub_off(1)    = KS - 1 ! MPI start index starts with 0
     sub_off(2)    = 0
     sub_off(3)    = 0
-
     call MPI_Type_create_subarray(3, sizes, subsizes, sub_off, order, etype, centerTypeZXY, err)
     call MPI_Type_commit(centerTypeZXY, err)
+
+    ! for axistype == 'ZHXY'
+    startZHXY(1)   = 1
+    startZHXY(2:3) = startXY(1:2)
+    countZHXY(1)   = KMAX+1
+    countZHXY(2:3) = countXY(1:2)
+    ! construct MPI subarray data type
+    sizes(1)      = KA
+    sizes(2)      = IA
+    sizes(3)      = JA
+    subsizes(1)   = KMAX+1
+    subsizes(2)   = IA
+    subsizes(3)   = JA
+    sub_off(1)    = KS - 2 ! MPI start index starts with 0
+    sub_off(2)    = 0
+    sub_off(3)    = 0
+    call MPI_Type_create_subarray(3, sizes, subsizes, sub_off, order, etype, centerTypeZHXY, err)
+    call MPI_Type_commit(centerTypeZHXY, err)
+
 
     ! for axistype == 'Land'
     startLAND(1)   = 1
@@ -510,7 +531,6 @@ contains
     sizes(1)       = LKMAX
     subsizes(1)    = LKMAX
     sub_off(1)     = LKS - 1 ! MPI start index starts with 0
-
     call MPI_Type_create_subarray(3, sizes, subsizes, sub_off, order, etype, centerTypeLAND, err)
     call MPI_Type_commit(centerTypeLAND, err)
 
@@ -523,7 +543,6 @@ contains
     sizes(1)        = UKMAX
     subsizes(1)     = UKMAX
     sub_off(1)      = UKS - 1 ! MPI start index starts with 0
-
     call MPI_Type_create_subarray(3, sizes, subsizes, sub_off, order, etype, centerTypeURBAN, err)
     call MPI_Type_commit(centerTypeURBAN, err)
 
@@ -539,7 +558,6 @@ contains
     subsizes(2) = IMAXB
     sub_off(1)  = KHALO   ! MPI start index starts with 0
     sub_off(2)  = ISB - 1 ! MPI start index starts with 0
-
     call MPI_Type_create_subarray(2, sizes, subsizes, sub_off, order, etype, centerTypeZX, err)
     call MPI_Type_commit(centerTypeZX, err)
 
@@ -560,6 +578,7 @@ contains
     if( centerTypeXY    /= MPI_DATATYPE_NULL ) call MPI_Type_free(centerTypeXY,    err)
     if( centerTypeZX    /= MPI_DATATYPE_NULL ) call MPI_Type_free(centerTypeZX,    err)
     if( centerTypeZXY   /= MPI_DATATYPE_NULL ) call MPI_Type_free(centerTypeZXY,   err)
+    if( centerTypeZHXY  /= MPI_DATATYPE_NULL ) call MPI_Type_free(centerTypeZHXY,  err)
     if( centerTypeLAND  /= MPI_DATATYPE_NULL ) call MPI_Type_free(centerTypeLAND,  err)
     if( centerTypeURBAN /= MPI_DATATYPE_NULL ) call MPI_Type_free(centerTypeURBAN, err)
 
@@ -907,9 +926,14 @@ contains
        ! read data and halos into the local buffer
        ! Because KHALO is not saved in files, we use mpi derived datatypes to
        ! describe the layout of local read buffer
-       if    ( axistype == 'ZXY' ) then
+       if(      axistype == 'ZXY'  &
+           .or. axistype == 'ZXHY' &
+           .or. axistype == 'ZXYH' ) then
           call FileRead( var, fid, varname, step,                                      &
                          ntypes=1, dtype=centerTypeZXY, start=startZXY, count=countZXY )
+       elseif( axistype == 'ZHXY' ) then
+          call FileRead( var, fid, varname, step,                                      &
+                         ntypes=1, dtype=centerTypeZHXY, start=startZHXY, count=countZHXY )
        elseif( axistype == 'XYT' ) then
           startXY(3) = 1
           countXY(3) = step
@@ -1012,11 +1036,18 @@ contains
 
     if ( IO_AGGREGATE ) then
        ! read data and halos into the local buffer
-       if ( axistype == 'ZXYT' ) then
+       if (      axistype == 'ZXYT'  &
+            .or. axistype == 'ZXHYT' &
+            .or. axistype == 'ZXYHT' ) then
           startZXY(4) = 1
           countZXY(4) = step
           call FileRead( var, fid, varname, step,                                         &
                          ntypes=step, dtype=centerTypeZXY, start=startZXY, count=countZXY )
+       elseif ( axistype == 'ZHXYT' ) then
+          startZXY(4) = 1
+          countZXY(4) = step
+          call FileRead( var, fid, varname, step,                                         &
+                         ntypes=step, dtype=centerTypeZHXY, start=startZHXY, count=countZHXY )
        else
           write(*,*) 'xxx [FILEIO_read_var_4D] unsupported axis type. Check! axistype:', trim(axistype), ', item:',trim(varname)
           call PRC_MPIstop