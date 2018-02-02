6af2b508ca96af1a6e4cb90adb4c6eb87b04ad52
Hisashi Yashiro <h.yashiro@riken.jp>
2018-02-02 13:49:41 +0900

    Update testcase hydrostatic/500m_topo

--- scale-rm/test/case/hydrostatic/500m_topo/visualize/visualize.sh
+++ scale-rm/test/case/hydrostatic/500m_topo/visualize/visualize.sh
@@ -22,8 +22,8 @@ rm -f energy.dat mass.dat
 echo "+visualize by gpview"
 rm -f dcl.pdf
 
-var__set=(W)
-rangeset=(-1e-12:1e-12)
+var__set=(W WA WZ WP MOMZ MOMZ_Z MOMZ_P)
+rangeset=
 time_set=
 
 i=0
@@ -36,7 +36,65 @@ do
    fi
 
    # time series
-   gpview history.pe\*.nc@${var},x=20000,y=20000 --nocont --exch --wsn 2 || exit
+   gpview history.pe\*.nc@${var},x=30000,y=20000 --nocont --exch --wsn 2 || exit
+   convert -density 150 -rotate 90 +antialias dcl.pdf slice_${var}.png
+   rm -f dcl.pdf
+
+   # snapshot
+   for sec in ${time_set[@]}
+   do
+       gpview history.pe\*.nc@${var},time=${sec} --nocont --mean y ${range} --wsn 2 || exit
+       convert -density 150 -rotate 90 +antialias dcl.pdf ${var}${sec}sec.png
+       rm -f dcl.pdf
+   done
+
+   let i="${i} + 1"
+done
+
+var__set=(MOMX MOMX_Z MOMX_P)
+rangeset=
+time_set=
+
+i=0
+for var in ${var__set[@]}
+do
+   if [ ${rangeset[$i]} == "auto" ]; then
+      range=""
+   else
+      range="--range="${rangeset[$i]}
+   fi
+
+   # time series
+   gpview history.pe\*.nc@${var},xh=30000,y=20000 --nocont --exch --wsn 2 || exit
+   convert -density 150 -rotate 90 +antialias dcl.pdf slice_${var}.png
+   rm -f dcl.pdf
+
+   # snapshot
+   for sec in ${time_set[@]}
+   do
+       gpview history.pe\*.nc@${var},time=${sec} --nocont --mean y ${range} --wsn 2 || exit
+       convert -density 150 -rotate 90 +antialias dcl.pdf ${var}${sec}sec.png
+       rm -f dcl.pdf
+   done
+
+   let i="${i} + 1"
+done
+
+var__set=(MOMY MOMY_Z MOMY_P)
+rangeset=
+time_set=
+
+i=0
+for var in ${var__set[@]}
+do
+   if [ ${rangeset[$i]} == "auto" ]; then
+      range=""
+   else
+      range="--range="${rangeset[$i]}
+   fi
+
+   # time series
+   gpview history.pe\*.nc@${var},x=30000,yh=20000 --nocont --exch --wsn 2 || exit
    convert -density 150 -rotate 90 +antialias dcl.pdf slice_${var}.png
    rm -f dcl.pdf
 
