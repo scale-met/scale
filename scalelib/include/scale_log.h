0f2aabad394d31b8ea0170d2c5951e469fcf1e92
Hisashi Yashiro <h.yashiro@riken.jp>
2018-04-09 15:48:09 +0900

    Modify log MACRO

--- scalelib/include/scale_log.h
+++ scalelib/include/scale_log.h
@@ -2,24 +2,24 @@
 #define SCALE_LOG_H
 
 #define LOG_ERROR(subroutine_name,format) \
-  write(*,'(3A)',advance='no') "ERROR [",subroutine_name,"] "; \
+  write(*,'(3A)',advance='no') "ERROR [",subroutine_name,"]"; \
   write(*,format)
 #define LOG_ERROR_CONT(format) \
-  write(*,'(3x)',advance='no'); \
+  write(*,'(5x)',advance='no'); \
   write(*,format)
 
 
 #define LOG_WARN(subroutine_name,format) \
   if (IO_L) write(IO_FID_LOG,*); \
-  if (IO_L) write(IO_FID_LOG,'(3A)',advance='no') "WARN  [",subroutine_name,"] "; \
+  if (IO_L) write(IO_FID_LOG,'(3A)',advance='no') "WARN  [",subroutine_name,"]"; \
   if (IO_L) write(IO_FID_LOG,format)
 
 
 #define LOG_INFO(subroutine_name,format) \
-  if (IO_L) write(IO_FID_LOG,'(3A)',advance='no') "INFO  [", subroutine_name, "] "; \
+  if (IO_L) write(IO_FID_LOG,'(3A)',advance='no') "INFO  [", subroutine_name, "]"; \
   if (IO_L) write(IO_FID_LOG,format)
 #define LOG_INFO_CONT(format) \
-  if (IO_L) write(IO_FID_LOG,'(3x)',advance='no'); \
+  if (IO_L) write(IO_FID_LOG,'(5x)',advance='no'); \
   if (IO_L) write(IO_FID_LOG,format)
 
 #define LOG_NEWLINE \
@@ -27,7 +27,7 @@
 
 
 #define LOG_PROGRESS(format) \
-  if (IO_L) write(IO_FID_LOG,'(1x,A)',advance='no') "+++ "; \
+  if (IO_L) write(IO_FID_LOG,'(A)',advance='no') "+++++"; \
   if (IO_L) write(IO_FID_LOG,format)
 
 
