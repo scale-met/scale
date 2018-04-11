9896b58786737500f2ee98080b388b751e020f95
Seiya Nishizawa <seiya@gfd-dennou.org>
2018-04-11 15:08:02 +0900

    fixed potential zero division and wrong openmp directive in calculation of the Obukhov length

--- scale-rm/src/atmos/mod_atmos_phy_sf_driver.F90
+++ scale-rm/src/atmos/mod_atmos_phy_sf_driver.F90
@@ -116,6 +116,7 @@ contains
   !> calculation tendency
   subroutine ATMOS_PHY_SF_driver_calc_tendency( update_flag )
     use scale_const, only: &
+       EPS    => CONST_EPS,    &
        GRAV   => CONST_GRAV,   &
        KARMAN => CONST_KARMAN, &
        CPdry  => CONST_CPdry
@@ -233,7 +234,7 @@ contains
 
        if ( .NOT. CPL_sw ) then
 
-          !omp parallel do
+          !$omp parallel do
           do j = JSB, JEB
           do i = ISB, IEB
              ATM_W   (i,j) = W   (KS,i,j)
@@ -280,20 +281,21 @@ contains
        endif
 
        ! temtative
-       !$omp parallel do
+       !$omp parallel do private(us,sflx_pt)
        do j = JS, JE
        do i = IS, IE
           us = max( 1.E-6_RP, &
                     sqrt( sqrt( SFLX_MU(i,j)**2 + SFLX_MV(i,j)**2 ) / DENS(KS,i,j) ) ) ! frictional velocity
           SFLX_PT = SFLX_SH(i,j) / ( CPdry * DENS(KS,i,j) ) &
                   * POTT(KS,i,j) / TEMP(KS,i,j)
+          SFLX_PT = sign( max(abs(SFLX_PT), EPS), SFLX_PT )
           l_mo(i,j) = - us**3 * POTT(KS,i,j) / ( KARMAN * GRAV * SFLX_PT )
        end do
        end do
 
        call history_output
 
-       !omp parallel do
+       !$omp parallel do
 !OCL XFILL
        do j = JS, JE
        do i = IS, IE
@@ -305,7 +307,7 @@ contains
        enddo
 
        if ( .not. ATMOS_HYDROMETEOR_dry ) then
-          !omp parallel do
+          !$omp parallel do
           do j = JS, JE
           do i = IS, IE
              work = SFLX_QTRC(i,j,I_QV) / ( FZ(KS,i,j) - FZ(KS-1,i,j) )
@@ -318,7 +320,7 @@ contains
 
     endif
 
-    !omp parallel do
+    !$omp parallel do
     do j = JS, JE
     do i = IS, IE
        MOMZ_t(KS,i,j) = MOMZ_t(KS,i,j) + MOMZ_t_SF(i,j)
@@ -329,9 +331,9 @@ contains
     enddo
 
     if ( .not. ATMOS_HYDROMETEOR_dry ) then
-       !omp parallel do
-       do j  = JS, JE
-       do i  = IS, IE
+       !$omp parallel do
+       do j = JS, JE
+       do i = IS, IE
           DENS_t(KS,i,j) = DENS_t(KS,i,j) + DENS_t_SF(i,j)
           RHOQ_t(KS,i,j,I_QV) = RHOQ_t(KS,i,j,I_QV) + RHOQ_t_SF(i,j,I_QV)
           RHOT_t(KS,i,j) = RHOT_t(KS,i,j) + RHOT_t_SF(i,j)
