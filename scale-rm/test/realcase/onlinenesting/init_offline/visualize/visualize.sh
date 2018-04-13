4c6d70c3873db5a2f40b078bb59c426579ed2fd4
Hisashi Yashiro <h.yashiro@riken.jp>
2018-04-13 17:36:52 +0900

    Add offline variants in onlinenesting

---
+++ scale-rm/test/realcase/onlinenesting/init_offline/visualize/visualize.sh
@@ -0,0 +1,69 @@
+#! /bin/bash -x
+
+### Visalization ###
+echo "+visualize by gpview"
+rm -f dcl.pdf
+
+for domain in d02
+do
+
+# initial conditions
+var__set=(DENS MOMZ MOMX MOMY RHOT QV  )
+rangeset=(auto auto auto auto auto auto)
+
+i=0
+for var in ${var__set[@]}
+do
+   if [ ${rangeset[$i]} == "auto" ]; then
+      range=""
+   else
+      range="--range="${rangeset[$i]}
+   fi
+
+   if [ ${var} == "MOMZ" ]; then
+      level="zh"
+   else
+      level="z"
+   fi
+
+   # snapshot
+   gpview init_${domain}_*.pe\*.nc@${var},${level}=0 --nocont --aspect 1 ${range} --wsn 2 || exit
+   convert -density 150 -rotate 90 +antialias dcl.pdf init.${var}_${domain}.png
+   rm -f dcl.pdf
+
+   let i="${i} + 1"
+done
+
+done
+
+for domain in d02
+do
+
+# boundary conditions
+var__set=(DENS VELZ VELX VELY POTT QV  )
+rangeset=(auto auto auto auto auto auto)
+
+i=0
+for var in ${var__set[@]}
+do
+   if [ ${rangeset[$i]} == "auto" ]; then
+      range=""
+   else
+      range="--range="${rangeset[$i]}
+   fi
+
+   if [ ${var} == "VELZ" ]; then
+      level="zh"
+   else
+      level="z"
+   fi
+
+   # average
+   gpview boundary_${domain}.pe\*.nc@${var},${level}=0 --nocont --aspect 1 --mean time ${range} --wsn 2 || exit
+   convert -density 150 -rotate 90 +antialias dcl.pdf bnd.${var}_${domain}.png
+   rm -f dcl.pdf
+
+   let i="${i} + 1"
+done
+
+done
---
+++ scale-rm/test/realcase/onlinenesting/run_offline/visualize/energy_flx.plt
@@ -0,0 +1,16 @@
+set terminal png size 1280,640
+set output "Energy_FLX.png"
+
+set xlabel "Step"
+set ylabel "Energy[J]"
+
+set multiplot layout 3,2 title "Flux conservation check"
+
+plot "energy_flx.dat" using 1:2          ti "Total Flux(ENGFLXT)"                 with linespoints pointsize 0.2
+plot "energy_flx.dat" using 1:($3+$4+$5) ti "Net SFC Flux"                        with linespoints pointsize 0.2
+plot "energy_flx.dat" using 1:3          ti "Sensible heat Flux (ENGFLX\\_SH)"    with linespoints pointsize 0.2
+plot "energy_flx.dat" using 1:4          ti "Latent   heat Flux (ENGFLX\\_LH)"    with linespoints pointsize 0.2
+plot "energy_flx.dat" using 1:5          ti "Net SFC Radiation Flux(ENGSFC\\_RD)" with linespoints pointsize 0.2
+plot "energy_flx.dat" using 1:6          ti "Net TOA Radiation Flux(ENGTOA\\_RD)" with linespoints pointsize 0.2
+
+unset multiplot
