2933a75395cab7e056b84415ff25dc0ca6930e96
Hisashi Yashiro <h.yashiro@riken.jp>
2018-04-23 10:21:16 +0900

    Change aspect ratio in visualize.sh of the test cases

--- scale-rm/test/framework/cnvtopo/GTOPO30_Antarctica/visualize/visualize.sh
+++ scale-rm/test/framework/cnvtopo/GTOPO30_Antarctica/visualize/visualize.sh
@@ -5,6 +5,6 @@ echo "+visualize by gpview"
 rm -f dcl.pdf
 
 # boundary conditions
-gpview topo.pe\*.nc@TOPO --nocont --aspect=4 --wsn 2 --srange=10: || exit
+gpview topo.pe\*.nc@TOPO --nocont --aspect=1 --wsn 2 --srange=10: || exit
 convert -density 150 -rotate 90 +antialias dcl.pdf topo.png
 rm -f dcl.pdf
