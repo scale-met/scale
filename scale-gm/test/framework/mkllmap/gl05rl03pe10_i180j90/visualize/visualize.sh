e20941654e1d153dc2d27d9d64c41f29e3ade798
Hisashi Yashiro <h.yashiro@riken.jp>
2018-08-30 19:53:05 +0900

    Tiny bugfix

--- scale-gm/test/framework/mkllmap/gl05rl03pe10_i180j90/visualize/visualize.sh
+++ scale-gm/test/framework/mkllmap/gl05rl03pe10_i180j90/visualize/visualize.sh
@@ -12,11 +12,11 @@ gpview sample1.nc@sample1 --nocont --itr=31 --map=coast_world --map_axis=0,-90,0
 convert -density 150 -rotate 90 +antialias dcl.pdf rank_sp.png
 rm -f dcl.pdf
 
-gpview sample2.nc@sample2 --nocont --itr=31 --map=coast_world --map_axis=0,90,0  --tone=b --int=0.5 --wsn 2 || exit
+gpview sample2.nc@sample2 --nocont --itr=31 --map=coast_world --map_axis=0,90,0  --tone=b           --wsn 2 || exit
 convert -density 150 -rotate 90 +antialias dcl.pdf region_np.png
 rm -f dcl.pdf
 
-gpview sample2.nc@sample2 --nocont --itr=31 --map=coast_world --map_axis=0,-90,0 --tone=b --int=0.5 --wsn 2 || exit
+gpview sample2.nc@sample2 --nocont --itr=31 --map=coast_world --map_axis=0,-90,0 --tone=b           --wsn 2 || exit
 convert -density 150 -rotate 90 +antialias dcl.pdf region_sp.png
 rm -f dcl.pdf
 
