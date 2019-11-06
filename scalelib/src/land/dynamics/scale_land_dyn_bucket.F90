f2422198a7754693cb60b54fb916b392180c00c3
Seiya Nishizawa <seiya@gfd-dennou.org>
2019-11-06 14:19:32 +0900

    tentative fix to avoid artificial temperature decreasing with snow precipitation

--- scalelib/src/land/dynamics/scale_land_dyn_bucket.F90
+++ scalelib/src/land/dynamics/scale_land_dyn_bucket.F90
@@ -180,6 +180,7 @@ contains
        TEMP, WATER,                   &
        RUNOFF                         )
     use scale_const, only: &
+       TEM00 => CONST_TEM00, &
        DWATR => CONST_DWATR, &
        EMELT => CONST_EMELT
     use scale_prc, only: &
@@ -228,6 +229,8 @@ contains
 
     real(RP) :: NDG_TEMP (LKMAX,LIA,LJA)
     real(RP) :: NDG_WATER(LKMAX,LIA,LJA)
+
+    real(RP) :: Q
 
     integer :: k, i, j
     !---------------------------------------------------------------------------
@@ -390,8 +393,11 @@ contains
       ! input from atmosphere
       do j = LJS, LJE
       do i = LIS, LIE
+         ! tentative (MUST consider ice water explicitly)
+         Q = SFLX_GH(i,j)
+         if ( TEMP(LKS,i,j) >= TEM00 ) Q = Q - SFLX_ice(i,j) * EMELT
          V(LKS,i,j) = TEMP(LKS,i,j) + NDG_TEMP(LKS,i,j) + TEMP_t(LKS,i,j) &
-                    + ( SFLX_GH(i,j) - SFLX_ice(i,j) * EMELT ) / ( LAND_DENSCS(LKS,i,j) * CDZ(LKS) ) * dt
+                    + Q / ( LAND_DENSCS(LKS,i,j) * CDZ(LKS) ) * dt
       end do
       end do
 
