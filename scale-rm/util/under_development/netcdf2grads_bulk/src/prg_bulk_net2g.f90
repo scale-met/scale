db8b882ded243b4f79c46e45870ce8b1efd40c3f
Hisashi Yashiro <h.yashiro@riken.jp>
2019-11-18 08:51:48 +0900

    Add more information to error log output (cont.)

--- scale-rm/util/under_development/netcdf2grads_bulk/src/prg_bulk_net2g.f90
+++ scale-rm/util/under_development/netcdf2grads_bulk/src/prg_bulk_net2g.f90
@@ -19,14 +19,17 @@ program netcdf2grads_h_launcher
   use scale_io
   use scale_prof
 
-  use scale_process, only: &
+  use scale_prc, only: &
      PRC_DOMAIN_nlim,     &
+     PRC_COMM_NULL,       &
+     PRC_abort,           &
      PRC_MPIstart,        &
-     PRC_MPIstop,         &
      PRC_MPIfinish,       &
-     PRC_MPIsplit,        &
+     PRC_MPIsplit_bulk,   &
+     PRC_MPIsplit_nest,   &
      PRC_UNIVERSAL_setup, &
-     PRC_GLOBAL_setup
+     PRC_GLOBAL_setup,    &
+     PRC_GLOBAL_ROOT
   use mod_net2g_comm
   use mod_netcdf2grads_h
   !-----------------------------------------------------------------------------
@@ -58,24 +61,24 @@ program netcdf2grads_h_launcher
      LOG_SPLIT,       &
      COLOR_REORDER
 
-  integer               :: universal_comm                         ! universal communicator
-  integer               :: universal_nprocs                       ! number of procs in universal communicator
-  integer               :: universal_myrank                       ! my rank         in universal communicator
-  logical               :: universal_master                       ! master process  in universal communicator?
-  character(len=H_LONG) :: universal_cnf_fname                    ! config file for launcher
+  integer               :: universal_comm                   ! universal communicator
+  integer               :: universal_nprocs                 ! number of procs in universal communicator
+  integer               :: universal_myrank                 ! my rank         in universal communicator
+  logical               :: universal_master                 ! master process  in universal communicator?
+  character(len=H_LONG) :: universal_cnf_fname              ! config file for launcher
 
-  integer               :: global_comm                            ! communicator for each member
-  integer               :: global_nprocs                          ! number of procs in global communicator
-  integer               :: PRC_BULKJOB(PRC_DOMAIN_nlim) = 0       ! = global_nprocs
-  character(len=H_LONG) :: dummy1     (PRC_DOMAIN_nlim) = ""
-  integer               :: intercomm_parent_null                  ! NULL inter communicator with parent
-  integer               :: intercomm_child_null                   ! NULL inter communicator with child
-  character(len=H_LONG) :: bulk_prefix                            ! dirname of each member
+  integer               :: global_comm                      ! communicator for each member
+  integer               :: global_nprocs                    ! number of procs in global communicator
+  integer               :: PRC_BULKJOB(PRC_DOMAIN_nlim) = 0 ! number of procs in each bulk job = global_nprocs
+  integer               :: ID_BULKJOB                       ! bulk job ID
 
-  integer               :: local_comm       ! assigned local communicator
-  integer               :: intercomm_parent ! inter communicator with parent
-  integer               :: intercomm_child  ! inter communicator with child
-  character(len=H_LONG) :: local_cnf_fname  ! config file for local domain
+  logical               :: use_fpm = .false.                ! switch for fpm module
+
+  integer               :: local_comm                       ! assigned local communicator
+  integer               :: ID_DOMAIN                        ! domain ID
+  integer               :: intercomm_parent                 ! inter communicator with parent
+  integer               :: intercomm_child                  ! inter communicator with child
+  character(len=H_LONG) :: local_cnf_fname                  ! config file for local domain
 
   integer :: fid, ierr
   !-----------------------------------------------------------
@@ -87,8 +90,6 @@ program netcdf2grads_h_launcher
                             universal_nprocs, & ! [OUT]
                             universal_myrank, & ! [OUT]
                             universal_master  ) ! [OUT]
-
-  call IO_set_rank( universal_myrank ) ! [IN]
 
   if( universal_master ) write(*,*) '*** Start Launch System for SCALE (POPSCA)'
 
@@ -109,7 +110,7 @@ program netcdf2grads_h_launcher
      ! keep default setting (no members, no nesting)
   elseif( ierr > 0 ) then !--- fatal error
      if( universal_master ) write(*,*) 'xxx Not appropriate names in namelist PARAM_LAUNCHER. Check!'
-     call PRC_MPIstop
+     call PRC_abort
   endif
 
   close(fid)
@@ -120,7 +121,7 @@ program netcdf2grads_h_launcher
      if( universal_master ) write(*,*) 'xxx Total Num of Processes must be divisible by NUM_BULKJOB. Check!'
      if( universal_master ) write(*,*) 'xxx Total Num of Processes = ', universal_nprocs
      if( universal_master ) write(*,*) 'xxx            NUM_BULKJOB = ', NUM_BULKJOB
-     call PRC_MPIstop
+     call PRC_abort
   endif
 
   global_nprocs = universal_nprocs / NUM_BULKJOB
@@ -131,17 +132,12 @@ program netcdf2grads_h_launcher
   endif
 
   ! communicator split for bulk/ensemble
-  call PRC_MPIsplit( universal_comm,        & ! [IN]
-                     NUM_BULKJOB,           & ! [IN]
-                     PRC_BULKJOB(:),        & ! [IN]
-                     dummy1 (:),            & ! [IN]  dummy
-                     LOG_SPLIT,             & ! [IN]
-                     .true.,                & ! [IN]  flag bulk_split
-                     .false.,               & ! [IN]  no reordering
-                     global_comm,           & ! [OUT]
-                     intercomm_parent_null, & ! [OUT] null
-                     intercomm_child_null,  & ! [OUT] null
-                     bulk_prefix            ) ! [OUT] dir name instead of file name
+  call PRC_MPIsplit_bulk( universal_comm, & ! [IN]
+                          NUM_BULKJOB,    & ! [IN]
+                          PRC_BULKJOB(:), & ! [IN]
+                          LOG_SPLIT,      & ! [IN]
+                          global_comm,    & ! [OUT]
+                          ID_BULKJOB      ) ! [OUT]
 
   call PRC_GLOBAL_setup( ABORT_ALL_JOBS, & ! [IN]
                          global_comm     ) ! [IN]
@@ -154,29 +150,35 @@ program netcdf2grads_h_launcher
   endif
 
   ! communicator split for nesting domains
-  call PRC_MPIsplit( global_comm,      & ! [IN]
-                     NUM_DOMAIN,       & ! [IN]
-                     PRC_DOMAINS(:),   & ! [IN]
-                     CONF_FILES (:),   & ! [IN]
-                     LOG_SPLIT,        & ! [IN]
-                     .false.,          & ! [IN] flag bulk_split
-                     COLOR_REORDER,    & ! [IN]
-                     local_comm,       & ! [OUT]
-                     intercomm_parent, & ! [OUT]
-                     intercomm_child,  & ! [OUT]
-                     local_cnf_fname   ) ! [OUT]
+  call PRC_MPIsplit_nest( global_comm,      & ! [IN]
+                          NUM_DOMAIN,       & ! [IN]
+                          PRC_DOMAINS(:),   & ! [IN]
+                          LOG_SPLIT,        & ! [IN]
+                          COLOR_REORDER,    & ! [IN]
+                          local_comm,       & ! [OUT]
+                          ID_DOMAIN,        & ! [OUT]
+                          intercomm_parent, & ! [OUT]
+                          intercomm_child   ) ! [OUT]
+
+  call IO_set_globalrank( universal_myrank, & ! [IN]
+                          ID_BULKJOB,       & ! [IN]
+                          ID_DOMAIN         ) ! [IN]
 
   !--- start main routine
 
   if ( NUM_BULKJOB > 1 ) then
-     local_cnf_fname = trim(bulk_prefix)//"/"//trim(local_cnf_fname)
+     write(local_cnf_fname,'(I4.4,2A)') ID_BULKJOB, "/", trim(CONF_FILES(ID_DOMAIN))
+  else
+     local_cnf_fname = trim(CONF_FILES(ID_DOMAIN))
   endif
 
   call popsca( local_comm,     & ! [IN]
                local_cnf_fname ) ! [IN]
 
   ! stop MPI
-  call comm_finalize
+  call PRC_MPIfinish
+
+  if( universal_master ) write(*,*) '*** End   Launch System for SCALE (POPSCA)'
 
   stop
 end program netcdf2grads_h_launcher
