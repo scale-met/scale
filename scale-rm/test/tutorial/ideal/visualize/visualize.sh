1b29f843e9cad0badc8a0bb56d06a09cd5d0e2b4
Seiya Nishizawa <seiya@gfd-dennou.org>
2020-01-28 14:58:06 +0900

    update the configs for the tutorial ideal case

--- scale-rm/test/tutorial/ideal/visualize/visualize.sh
+++ scale-rm/test/tutorial/ideal/visualize/visualize.sh
@@ -12,7 +12,7 @@ time_set=(0600 1200 1800 2400 3000 3600)
 # snapshot
 for sec in ${time_set[@]}
 do
-    gpvect --scalar --slice z=0:16000,y=0,time=${sec} --nocont --noflow_vect --unit_vect --aspect=1.0 --xscale=20 --yscale=20 --xintv=8 --yintv=8 history.pe\*.nc@QHYD history.pe\*.nc@U history.pe\*.nc@W --wsn 2 || exit
+    gpvect --scalar --slice z=0:16000,x=0,time=${sec} --nocont --noflow_vect --unit_vect --aspect=1.0 --xscale=20 --yscale=20 --xintv=8 --yintv=8 history.pe\*.nc@QHYD history.pe\*.nc@V history.pe\*.nc@W --wsn 2 || exit
     convert -density 150 -rotate 90 +antialias dcl.pdf QHYD${sec}sec.png
     rm -f dcl.pdf
 done
