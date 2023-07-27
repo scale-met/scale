f85f3bc069798b1124689c32a45ae2f1109b83d8
Seiya Nishizawa <seiya@gfd-dennou.org>
2023-07-27 13:20:45 +0900

    change the way to specify LSIZE

--- scalelib/include/scalelib.h
+++ scalelib/include/scalelib.h
@@ -4,27 +4,16 @@
 #include "scale_log.h"
 #include "scale_openmp.h"
 
-#ifndef VECTLEN
 #ifdef _OPENACC
-#define VECTLEN 32
+  #define LSIZE 1
 #else
-#define VECTLEN 8
+  #ifndef LSIZE
+    #ifdef SINGLE
+      #define LSIZE 16
+    #else
+      #define LSIZE 8
+    #endif
+  #endif
 #endif
-#endif
-
-#ifndef CACHELINESIZE
-#define CACHELINESIZE 128
-#endif
-
-#ifdef _OPENACC
-#define LSIZE 1
-#else
-#ifdef SINGLE
-#define LSIZE (CACHELINESIZE / 8)
-#else
-#define LSIZE (CACHELINESIZE / 16)
-#endif
-#endif
-
 
 #endif
