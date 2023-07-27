1468a8016158413b96aada18b532045613162256
Sachiho A. Adachi <sachiho.adachi@riken.jp>
2023-07-27 17:42:34 +0900

    update the making tool for the complete settings of the experiment

--- scale-rm/test/tutorial/real/config/base.sno.conf.sh
+++ scale-rm/test/tutorial/real/config/base.sno.conf.sh
@@ -13,8 +13,8 @@ cat << EOF > base.sno.vgridope.conf
 /
 
 &PARAM_SNO
- basename_in     = '${RUNDIR}/history_d${FNUM}',
- dirpath_out     = '.',
+ basename_in     = '${RUNDIR}/${OUT_DIR}/history_d${FNUM}',
+ dirpath_out     = '${OUT_DIR_SNO}',
  basename_out    = 'merged-p_history_d${FNUM}',
  nprocs_x_out    = ${PRC_NUM_X[$D]},
  nprocs_y_out    = ${PRC_NUM_Y[$D]},
@@ -52,7 +52,7 @@ cat << EOF > base.sno.hgridope.conf
 
 &PARAM_SNO
  basename_in     = 'merged-p_history_d${FNUM}',
- dirpath_out     = '.',
+ dirpath_out     = '${OUT_DIR_SNO}',
  basename_out    = 'merged-h_history_d${FNUM}',
  nprocs_x_out    = 1,
  nprocs_y_out    = 1,
